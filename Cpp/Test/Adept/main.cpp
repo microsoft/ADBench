72f21b5054abc042305e0279025aaf700eff44d9
Filip Srajer <t-filsra@microsoft.com>
2015-09-01 15:37:00 +0100

    Adept BA ready

--- Cpp/Test/Adept/main.cpp
+++ Cpp/Test/Adept/main.cpp
@@ -3,8 +3,9 @@
 #include <chrono>
 #include <set>
 
-#define DO_GMM_FULL
+//#define DO_GMM_FULL
 //#define DO_GMM_SPLIT
+#define DO_BA
 
 #include "adept.h"
 #include "../utils.h"
@@ -13,6 +14,8 @@
 #if defined DO_GMM_FULL || defined DO_GMM_SPLIT
 #define ADEPT_COMPILATION
 #include "../ADOLC/gmm.h"
+#elif defined DO_BA
+#include "../ADOLC/ba.h"
 #endif
 
 using adept::adouble;
@@ -187,6 +190,106 @@ void test_gmm(const string& fn_in, const string& fn_out,
   write_times(fn_out + "_times_" + name + ".txt", tf, tJ);
 }
 
+#elif defined DO_BA
+double compute_ba_J(int nruns, int n, int m, int p,
+  double *cams, double *X, double *w, int *obs, double *feats,
+  double *reproj_err, double *w_err, BASparseMat *J)
+{
+  if (nruns == 0)
+    return 0.;
+
+  adept::Stack stack;
+  adouble acam[BA_NCAMPARAMS],
+    aX[3], aw, areproj_err[2], aw_err;
+  int n_new_cols = BA_NCAMPARAMS + 3 + 1;
+  vector<double> reproj_err_d(2 * n_new_cols);
+
+  high_resolution_clock::time_point start, end;
+  start = high_resolution_clock::now();
+  for (int i = 0; i < nruns; i++)
+  {
+    *J = BASparseMat(n, m, p);
+
+    for (int i = 0; i < p; i++)
+    {
+      memset(reproj_err_d.data(), 0, 2 * n_new_cols*sizeof(double));
+
+      int camIdx = obs[2 * i + 0];
+      int ptIdx = obs[2 * i + 1];
+      adept::set_values(acam, BA_NCAMPARAMS, &cams[BA_NCAMPARAMS*camIdx]);
+      adept::set_values(aX, 3, &X[ptIdx * 3]);
+      aw.set_value(w[i]);
+
+      stack.new_recording();
+      computeReprojError(acam, aX, &aw, &feats[2 * i], areproj_err);
+      stack.independent(acam, BA_NCAMPARAMS);
+      stack.independent(aX, 3);
+      stack.independent(aw);
+      stack.dependent(areproj_err, 2);
+      stack.jacobian_reverse(reproj_err_d.data());
+
+      adept::get_values(areproj_err, 2, &reproj_err[2 * i]);
+
+      J->insert_reproj_err_block(i, camIdx, ptIdx, reproj_err_d.data());
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+      aw.set_value(w[i]);
+      
+      stack.new_recording();
+      computeZachWeightError(&aw, &aw_err);
+      aw_err.set_gradient(1.);
+      stack.reverse();
+
+      w_err[i] = aw_err.value();
+      double err_d = aw.get_gradient();
+
+      J->insert_w_err_block(i, err_d);
+    }
+  }
+
+  end = high_resolution_clock::now();
+  double t_J = duration_cast<duration<double>>(end - start).count() / nruns;
+  cout << "t_J:" << t_J << endl;
+
+  return t_J;
+}
+
+void test_ba(const string& fn_in, const string& fn_out,
+  int nruns_f, int nruns_J)
+{
+  int n, m, p;
+  vector<double> cams, X, w, feats;
+  vector<int> obs;
+
+  read_ba_instance(fn_in + ".txt", n, m, p,
+    cams, X, w, obs, feats);
+
+  vector<double> reproj_err(2 * p);
+  vector<double> w_err(p);
+  BASparseMat J(n, m, p);
+
+  high_resolution_clock::time_point start, end;
+  double tf = 0., tJ = 0;
+
+  start = high_resolution_clock::now();
+  for (int i = 0; i < nruns_f; i++)
+  {
+    ba_objective(n, m, p, cams.data(), X.data(),
+      w.data(), obs.data(), feats.data(),
+      reproj_err.data(), w_err.data());
+  }
+  end = high_resolution_clock::now();
+  tf = duration_cast<duration<double>>(end - start).count() / nruns_f;
+
+  string name("Adept");
+  tJ = compute_ba_J(nruns_J, n, m, p, cams.data(), X.data(), w.data(),
+    obs.data(), feats.data(), reproj_err.data(), w_err.data(), &J);
+
+  //write_J_sparse(fn_in + "_J_" + name + ".txt", J);
+  write_times(fn_in + "_times_" + name + ".txt", tf, tJ);
+}
 #endif
 
 int main(int argc, char *argv[])
@@ -202,5 +305,7 @@ int main(int argc, char *argv[])
 
 #if defined DO_GMM_FULL || defined DO_GMM_SPLIT
   test_gmm(dir_in + fn, dir_out + fn, nruns_f, nruns_J, replicate_point);
+#elif defined DO_BA
+  test_ba(dir_in + fn, dir_out + fn, nruns_f, nruns_J);
 #endif
 }
