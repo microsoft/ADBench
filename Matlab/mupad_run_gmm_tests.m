722dd5dd1338a60f2457d38bc58fdf9911d3aa53
Filip Srajer <t-filsra@microsoft.com>
2015-09-04 09:05:46 +0100

    mupad skip run if nruns==0

--- Matlab/mupad_run_gmm_tests.m
+++ Matlab/mupad_run_gmm_tests.m
@@ -23,6 +23,10 @@ for i=1:ntasks
     nruns_curr_f = nruns_f(i);
     nruns_curr_J = nruns_J(i);
     
+    if nruns_curr_f+nruns_curr_J == 0
+        continue;
+    end
+    
     if nruns_curr_f > 0
         tic
         [ J, err ] = mupad_gmm_objective(nruns_curr_f, paramsGMM, x, ...
