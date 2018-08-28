52e1c60b745d61aebb648e3dadabe3da789c7ca0
Zak Smith <spike.smith@hotmail.com>
2018-08-28 09:22:48 +0100

    Edit PyTorch wrapper code

--- tools/PyTorch/torch_wrapper.py
+++ tools/PyTorch/torch_wrapper.py
@@ -1,27 +1,26 @@
 import torch
 
 
+# Recursively call .backward on multi-dimensional output
+def recurse_backwards(output):
+    if output.dim() > 0:
+        for item in output:
+            recurse_backwards(item)
+    else:
+        output.backward(retain_graph=True)
+
+
 # Run a function with torch tensors
 def torch_func(func, inputs, params, do_J):
-    inputs = tuple(torch.tensor(inp, requires_grad=do_J) for inp in inputs)
-    params = tuple(torch.tensor(param) for param in params)
+    inputs = tuple(torch.tensor(inp, dtype=torch.float64, requires_grad=do_J) for inp in inputs)
+    params = tuple(torch.tensor(param, dtype=torch.float64) for param in params)
 
-    print("Start func")
     res = func(*inputs, *params)
-    print("End func")
 
     if do_J:
-        if res.dim() > 0:
-            for output in res:
-                print("Output.backward")
-                output.backward(retain_graph=True)
-                print("Get grads")
-                grad = tuple(inp.grad for inp in inputs)
-                print("GRAD:", grad)
-        else:
-            res.backward()
-            grad = tuple(inp.grad for inp in inputs)
+        recurse_backwards(res)
+        grad = tuple(inp.grad for inp in inputs)
     else:
         grad = None
-    print("Return grads")
+
     return res, grad
