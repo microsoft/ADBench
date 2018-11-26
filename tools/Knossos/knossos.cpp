c7ca5d2d598ff70f974b0b85299d123724dfdb28
Tom Ellis <toelli@microsoft.com>
2018-11-26 18:09:43 +0000

    Allocate much more memory for Knossos bump allocator
    because it does genuinely seem to require that much on Azure DevOps and
    an Azure VS VM.  I don't know why it didn't use that much when running on
    my desktop machine though.

--- tools/Knossos/knossos.cpp
+++ tools/Knossos/knossos.cpp
@@ -1,5 +1,8 @@
 #include "knossos.h"
 
 namespace ks {
-  allocator g_alloc(1000000);
+  // 1m seemed to work fine when I ran the benchmark on my local
+  // machine but to succeed on Azure DevOps it seems to need 10b.  I
+  // have no idea what the reason for that difference is.
+  allocator g_alloc(10 * 1000 * 1000 * 1000);
 }
