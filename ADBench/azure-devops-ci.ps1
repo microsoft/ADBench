50306179a300b9e2ba93b2d43b9cd706542f5a8c
Tom Ellis <tom.ellis@microsoft.com>
2019-01-14 15:23:28 +0000

    Add Azure DevOps build definition for all the tools that we know work

--- ADBench/azure-devops-ci.ps1
+++ ADBench/azure-devops-ci.ps1
@@ -1,4 +1,11 @@
-python -m pip install --user matplotlib plotly
+# NB Julia is astonishingly slow to install.  10 mins for Julia itself
+# and the two needed packages.  Eventually we should try to improve
+# that, but for now we will live with it.
+choco install --limit-output julia
+julia -e 'import Pkg; Pkg.add.([\"SpecialFunctions\", \"ForwardDiff\"])'
+
+python -m pip install --user matplotlib plotly numpy scipy autograd
+python -m pip install torch==1.2.0+cpu torchvision==0.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
 # I'm not sure that setting up vcvars is necessary in Azure Dev Ops.
 # We have a VS2017 host so presumably it has all the paths set up
@@ -23,7 +30,7 @@ cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$Env:BUILD_SOURCESDIRECTORY\toolchain.c
 cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$Env:BUILD_SOURCESDIRECTORY\toolchain.cmake" '-DCMAKE_BUILD_TYPE="RelWithDebInfo"' .
 
 ninja
-powershell .\ADBench\run-all.ps1 -repeat -nruns_J 10 -nruns_f 10 -time_limit 5
+powershell .\ADBench\run-all.ps1 -repeat -nruns_J 10 -nruns_f 10 -time_limit 5 -tools Manual,ManualEigen,Finite,PyTorch,Autograd,Julia -gmm_d_vals 2,10 -gmm_k_vals 5,10
 python ADBench/plot_graphs.py --save --plotly
 cp -r "tmp/graphs" "$Env:BUILD_ARTIFACTSTAGINGDIRECTORY"
 Write-Host "##vso[artifact.upload containerfolder=;artifactname=Artifact;localpath=$Env:BUILD_ARTIFACTSTAGINGDIRECTORY;]$Env:BUILD_ARTIFACTSTAGINGDIRECTORY"
