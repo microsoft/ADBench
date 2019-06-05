5a1e21b1ea92e6a4aea95eaa1f4335b4b0b52070
Alexander Novikov <b-alnovi@microsoft.com>
2019-06-05 13:30:43 +0300

    runner tries to load module

--- src/modules/cpp/manual/ManualGMM.cpp
+++ src/modules/cpp/manual/ManualGMM.cpp
@@ -1,6 +1,11 @@
 // ManualGMM.cpp : Defines the exported functions for the DLL.
 #include "ManualGMM.h"
 
+#include <iostream>
+#include<memory>
+
+using namespace std;
+
 // DLL internal state variables:
 //static unsigned long long previous_;  // Previous value, if any
 //static unsigned long long current_;   // Current sequence value
@@ -27,4 +32,11 @@ void ManualGMM::performAD(int times)
 void ManualGMM::output()
 {
 	//return some documented output
+	std::cout << "I am alive!" << endl;
+}
+
+__declspec(dllexport) unique_ptr<IGMMTest> __cdecl GetGMMTest()
+{
+	auto testPtr = unique_ptr<IGMMTest>(new ManualGMM());
+	return testPtr;
 }
