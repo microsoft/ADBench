34fb97c1935d4c78d297840bc7af9a8ca7360362
Alexander Novikov <b-alnovi@microsoft.com>
2019-06-06 15:43:50 +0300

    the project builds

--- test/runners/cpp/CppRunnerTests.cpp
+++ test/runners/cpp/CppRunnerTests.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
+#include "../../../src/runners/cpp/ModuleLoader.h"
 
-TEST(CppRunnerTests, TEST1) {
+TEST(CppRunnerTests, LibraryLoadTest) {
+	/*ModuleLoader moduleLoader("GMMMock.dll");
+	auto test = moduleLoader.GetTest();
+	EXPECT_EQ(test == NULL, true);*/
 	EXPECT_EQ(true, true);
 }
