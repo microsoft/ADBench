1fabd9faa14ee0758eda2f5c50eab2b59c3ce76b
EgorovIlia <egorovIlyak@yandex.ru>
2019-06-10 17:00:11 +0300

    implemented ManualGMM::prepare

--- src/modules/cpp/manual/ManualGMM.h
+++ src/modules/cpp/manual/ManualGMM.h
@@ -5,6 +5,13 @@
 #include "../../../runners/cpp/GMMData.h"
 
 class ManualGMM : public ITest<GMMInput, GMMOutput> {
+private:
+	int d = 0, k = 0, n = 0;
+	vector<double> alphas, means, icf, x, J;
+	Wishart wishart = { 0, 0 };
+	double err = 0;
+
+public:
 	// This function must be called before any other function.
 	virtual void prepare(GMMInput&& input) override;
 
@@ -16,6 +23,4 @@ class ManualGMM : public ITest<GMMInput, GMMOutput> {
 	virtual GMMOutput output() override; //TODO: should be not void
 
 	~ManualGMM() {}
-
-
 };
