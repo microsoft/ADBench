e3e4fa70803bd18f7052fba86646c2dfecc40142
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-06-10 13:12:09 +0300

    Fixed GTest in RelWithDebInfo configuration.

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,3 +3,7 @@ hunter_config(glog
   VERSION ${HUNTER_glog_VERSION} CMAKE_ARGS
 	BUILD_SHARED_LIBS=ON
 )
+
+# Making sure there will be GTest (when tests are enabled)
+# in RelWithDebInfo configuration to link against
+hunter_config(GTest VERSION ${HUNTER_GTest_VERSION} CONFIGURATION_TYPES Release RelWithDebInfo Debug)
