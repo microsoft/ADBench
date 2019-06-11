ab4269b648b15ff6cb4d1317534143c1a3b3f862
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-06-11 15:51:58 +0300

    Slight mod for manual gmm.

--- src/cpp/modules/manual/gmm_d.h
+++ src/cpp/modules/manual/gmm_d.h
@@ -10,13 +10,14 @@
 // means: d*k component means
 // icf: (d*(d+1)/2)*k inverse covariance factors 
 //					every icf entry stores firstly log of diagonal and then 
-//          columnwise other entris
+//          columnwise other entries
 //          To generate icf in MATLAB given covariance C :
 //              L = inv(chol(C, 'lower'));
 //              inv_cov_factor = [log(diag(L)); L(au_tril_indices(d, -1))]
 // wishart: wishart distribution parameters
 // x: d*n points
-// err: 1 output
+// err: objective function output
+// J: gradient output
 void gmm_objective_d(int d, int k, int n,
     const double *alphas,
     const double *means,
