7cfbf5dc4a6c7f2f23eeb24b792522cf5fcea0e0
Ilia Egorov <egorovIlyak@yandex.ru>
2019-06-17 15:59:11 +0300

    added manualEigenGMM

---
+++ src/cpp/modules/manualEigen/ManualEigenGMM.cpp
@@ -0,0 +1,41 @@
+// ManualGMM.cpp : Defines the exported functions for the DLL.
+#include "ManualGMMEigen.h"
+#include "../../shared/gmm.h"
+#include "gmm_eigen.h"
+
+#include <iostream>
+#include <memory>
+
+// This function must be called before any other function.
+void ManualEigenGMM::prepare(GMMInput&& input)
+{
+    _input = input;
+    int Jcols = (_input.k * (_input.d + 1) * (_input.d + 2)) / 2;
+	_output = { 0,  std::vector<double>(Jcols) };
+}
+
+GMMOutput ManualEigenGMM::output()
+{
+    return _output;
+}
+
+void ManualEigenGMM::calculateObjective(int times)
+{
+    for (int i = 0; i < times; ++i) {
+        gmm_objective(_input.d, _input.k, _input.n, _input.alphas.data(), _input.means.data(),
+            _input.icf.data(), _input.x.data(), _input.wishart, &_output.objective);
+    }
+}
+
+void ManualEigenGMM::calculateJacobian(int times)
+{
+    for (int i = 0; i < times; ++i) {
+        gmm_objective_d(_input.d, _input.k, _input.n, _input.alphas.data(), _input.means.data(),
+            _input.icf.data(), _input.x.data(), _input.wishart, &_output.objective, _output.gradient.data());
+    }
+}
+
+extern "C" __declspec(dllexport) ITest<GMMInput, GMMOutput>* __cdecl GetGMMTest()
+{
+    return new ManualEigenGMM();
+}
