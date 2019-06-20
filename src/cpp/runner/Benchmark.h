e7ae08bede61c7d03997826e3c180ee17b8d0ecd
Alexander Novikov <b-alnovi@microsoft.com>
2019-06-20 17:12:23 +0300

    general implementation of get_test is deleted

--- src/cpp/runner/Benchmark.h
+++ src/cpp/runner/Benchmark.h
@@ -22,10 +22,7 @@ void call_member_function(std::unique_ptr<ITest<Input, Output>>& ptr_to_object, 
 }
 
 template<class Input, class Output>
-unique_ptr<ITest<Input, Output>> get_test(const ModuleLoader& module_loader)
-{
-    throw exception("Cpp runner doesn't support such test type yet");
-}
+unique_ptr<ITest<Input, Output>> get_test(const ModuleLoader& module_loader) = delete;
 
 template<class Input, class Output>
 double measure_shortest_time(const double minimum_measurable_time, const int nruns, const double time_limit, std::unique_ptr<ITest<Input, Output>>& test, const test_member_function<Input, Output> func)
@@ -76,16 +73,13 @@ void run_benchmark(const char* const module_path, const std::string& input_filep
 
     test->prepare(std::move(inputs));
 
-
     const auto objective_time =
         measure_shortest_time(minimum_measurable_time, nruns_F, time_limit, test, &ITest<Input, Output>::calculateObjective);
 
     const auto derivative_time =
         measure_shortest_time(minimum_measurable_time, nruns_J, time_limit, test, &ITest<Input, Output>::calculateJacobian);
 
-
     const auto output = test->output();
-
 
     const auto input_basename = filepath_to_basename(input_filepath);
     const auto module_basename = filepath_to_basename(module_path);
