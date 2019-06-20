b792e018ee74cb6d89ebf3417b8ddb9cf5e1e1bb
Alexander Novikov <b-alnovi@microsoft.com>
2019-06-20 14:27:15 +0300

    output saving methods moved to source file

---
+++ src/cpp/runner/OutputSave.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include "../shared/utils.h"
+
+void save_time_to_file(const std::string& filepath, const double objective_time, const double derivative_time);
+
+void save_objective_to_file(const std::string& filepath, const double& value);
+
+void save_gradient_to_file(const std::string& filepath, const std::vector<double>& gradient);
+
+void save_errors_to_file(const std::string& filepath, const std::vector<double>& reprojection_error,
+                         const std::vector<double>& zach_weight_error);
+
+void save_sparse_J_to_file(const std::string& filepath, const BASparseMat& jacobian);
