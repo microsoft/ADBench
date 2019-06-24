3e9026a9ec5e18cd8f844ad884af439a5bf15763
Ilia Egorov <egorovIlyak@yandex.ru>
2019-06-24 13:44:48 +0300

    convert tabs to spaces

--- src/cpp/modules/manualEigen/gmm_d.h
+++ src/cpp/modules/manualEigen/gmm_d.h
@@ -6,10 +6,10 @@
 // k: number of gaussians
 // n: number of points
 // alphas: k logs of mixture weights (unnormalized), so
-//			weights = exp(log_alphas) / sum(exp(log_alphas))
+//            weights = exp(log_alphas) / sum(exp(log_alphas))
 // means: d*k component means
 // icf: (d*(d+1)/2)*k inverse covariance factors 
-//					every icf entry stores firstly log of diagonal and then 
+//                    every icf entry stores firstly log of diagonal and then 
 //          columnwise other entris
 //          To generate icf in MATLAB given covariance C :
 //              L = inv(chol(C, 'lower'));
