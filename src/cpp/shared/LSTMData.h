fddc0874703b7d61d2cc6cff0a69322042b08bf1
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-06-24 17:31:33 +0300

    C++ structures for Hand and LSTM i/o.

---
+++ src/cpp/shared/LSTMData.h
@@ -0,0 +1,19 @@
+#pragma once 
+
+#include <vector>
+
+struct LSTMInput
+{
+    int l;
+    int c;
+    int b;
+    std::vector<double> main_params;
+    std::vector<double> extra_params;
+    std::vector<double> state;
+    std::vector<double> sequence;
+};
+
+struct LSTMOutput {
+    double objective;
+    std::vector<double> gradient;
+};
