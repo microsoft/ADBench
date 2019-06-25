dc32780b3af246ac0dd815309adc96dd39037707
Alexander Novikov <b-alnovi@microsoft.com>
2019-06-25 17:48:20 +0300

    cpp runner comments

--- src/cpp/runner/Benchmark.h
+++ src/cpp/runner/Benchmark.h
@@ -10,20 +10,34 @@ using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 
+/* 
+ * General logic of the benchmark described in this file.
+ * However, functions responsible for data reading and outputting, 
+ * as well as for obtaining a test instance, must be implemented for each specific data type. 
+ * Such functions are implemented in <test_type>Benchmark.cpp files. 
+ */ 
+
+//Reads input_file into Input struct. replicate_point flag duplicates one vector over all input data. 
+//Templated function "read_input_data" is deleted to cause a link error if a corresponding template specialization is not implemented.
 template<class Input>
 Input read_input_data(const std::string& input_file, bool replicate_point) = delete;
 
+//Template of a pointer to a function which is a member of the ITest class and takes a single argument of int type. 
 template <class Input, class Output>
 using test_member_function = void (ITest<Input, Output>::*) (int);
 
+//This function calls a member of ITest class passing 1 int argument.
 template <class Input, class Output>
 void call_member_function(std::unique_ptr<ITest<Input, Output>>& ptr_to_object, const test_member_function<Input, Output> ptr_to_member, const int arg1) {
     (*(ptr_to_object).*(ptr_to_member))(arg1);
 }
 
+//Makes a request to the ModuleLoader to get a test of a desired type.
+//Templated function "get_test" is deleted to cause a link error if a corresponding template specialization is not implemented.
 template<class Input, class Output>
 unique_ptr<ITest<Input, Output>> get_test(const ModuleLoader& module_loader) = delete;
 
+//Measures time according to the documentation.
 template<class Input, class Output>
 double measure_shortest_time(const double minimum_measurable_time, const int nruns, const double time_limit, std::unique_ptr<ITest<Input, Output>>& test, const test_member_function<Input, Output> func)
 {
@@ -58,11 +72,13 @@ double measure_shortest_time(const double minimum_measurable_time, const int nru
     return min_sample;
 }
 
+//Templated function "save_output_to_file" is deleted to cause a link error if a corresponding template specialization is not implemented.
 template<class Output>
 void save_output_to_file(const Output& output, const string& output_prefix, const string& input_basename, const string& module_basename) = delete;
 
 std::string filepath_to_basename(const std::string& filepath);
 
+//Performs the entire benchmark process according to the documentation
 template<class Input, class Output>
 void run_benchmark(const char* const module_path, const std::string& input_filepath, const std::string& output_prefix, const double minimum_measurable_time, const int nruns_F, const int nruns_J,
     const double time_limit, const bool replicate_point) {
