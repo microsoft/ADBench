99b67bb5609e984ee75e12c3fc07f6cffcd9f5a5
Ilia Egorov <Egorovilyak@yandex.ru>
2019-06-28 17:40:50 +0300

    manualEigenHand CMake and Tests updated

--- test/cpp/modules/manualEigen/HandTests.cpp
+++ test/cpp/modules/manualEigen/HandTests.cpp
@@ -12,7 +12,7 @@ TEST(ManualEigenTests, Hand_Load) {
 	ASSERT_NE(test, nullptr);
 }
 
-TEST(ManualEigenTests, Hand_TestProcess)
+TEST(ManualEigenTests, Hand_TestProcessSimple)
 {
 #ifdef _DEBUG
 	ModuleLoader moduleLoader("../../../../src/cpp/modules/manualEigen/ManualEigend.dll");
@@ -24,29 +24,99 @@ TEST(ManualEigenTests, Hand_TestProcess)
 	HandInput input;
 
 	// Read instance
-    read_hand_instance("model", "handtest.txt", &input.theta, &input.data, &input.us);
+    read_hand_instance("model/", "handtestsmall.txt", &input.theta, &input.data);
 	module->prepare(std::move(input));
 	module->calculateObjective(1);
 	module->calculateJacobian(1);
-	/*auto output = module->output();
-	EXPECT_NEAR(8.0738, output.objective, 0.00001);
-	EXPECT_EQ(18, output.gradient.size());
-	EXPECT_NEAR(0.108663, output.gradient[0], 0.00001);
-	EXPECT_NEAR(-0.74127, output.gradient[1], 0.00001);
-	EXPECT_NEAR(0.632607, output.gradient[2], 0.00001);
-	EXPECT_NEAR(1.116926, output.gradient[3], 0.00001);
-	EXPECT_NEAR(0.163333, output.gradient[4], 0.00001);
-	EXPECT_NEAR(-0.022, output.gradient[5], 0.00001);
-	EXPECT_NEAR(0.227778, output.gradient[6], 0.00001);
-	EXPECT_NEAR(1.20963, output.gradient[7], 0.00001);
-	EXPECT_NEAR(-0.06064, output.gradient[8], 0.00001);
-	EXPECT_NEAR(2.5853, output.gradient[9], 0.00001);
-	EXPECT_NEAR(0.11263, output.gradient[10], 0.00001);
-	EXPECT_NEAR(0.38574, output.gradient[11], 0.00001);
-	EXPECT_NEAR(0.07352, output.gradient[12], 0.00001);
-	EXPECT_NEAR(5.41836, output.gradient[13], 0.00001);
-	EXPECT_NEAR(-0.3215, output.gradient[14], 0.00001);
-	EXPECT_NEAR(1.71892, output.gradient[15], 0.00001);
-	EXPECT_NEAR(0.86009, output.gradient[16], 0.00001);
-	EXPECT_NEAR(-0.99464, output.gradient[17], 0.00001);*/
+    auto output = module->output();
+
+    std::vector<double> expectedJ
+    {
+        -0.0195513, -0.00681028, -0.040936, -0.0710032, -0.0281347, -0.0254484,
+        0.0118421, 0.00174109, 0.0257836, 0.0514893, -0.0450592, 0.0623982,
+        -0.010814, -0.0654732, 0.00295529, -0.0633409, -0.0898189, 0.0207441,
+        -1, 0, 0, -1, 0, 0,
+        0, -1, 0, 0, -1, 0,
+        0, 0, -1, 0, 0, -1,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, -0.0345327, 0.0242025, -0.00310676,
+        0, 0, 0, -0.0195438, -0.0184393, 0.00652741,
+        0, 0, 0, -0.0129103, 0.00976606, 0.00849554,
+        0, 0, 0, 0.00199738, -0.00142571, -0.00043549,
+        -0.0221537, 0.0142669, -0.0312351, 0, 0, 0,
+        0.0298023, 0.025852, -0.0094043, 0, 0, 0,
+        -0.00889718, 0.00230413, -0.0184753, 0, 0, 0,
+        -0.00224888, 0.00309895, -0.000309212, 0, 0, 0
+    }; // Col-major
+
+    ASSERT_EQ(expectedJ.size(), output.jacobian.size());
+    for (int i = 0; i < expectedJ.size(); ++i)
+        EXPECT_NEAR(expectedJ[i], output.jacobian[i], 0.000001);
+}
+
+TEST(ManualEigenTests, Hand_TestProcessComplicated)
+{
+#ifdef _DEBUG
+    ModuleLoader moduleLoader("../../../../src/cpp/modules/manualEigen/ManualEigend.dll");
+#else
+    ModuleLoader moduleLoader("../../../../src/cpp/modules/manualEigen/ManualEigen.dll");
+#endif
+    auto module = moduleLoader.get_hand_test();
+    ASSERT_NE(module, nullptr);
+    HandInput input;
+
+    // Read instance
+    read_hand_instance("model/", "handtestcomplicated.txt", &input.theta, &input.data, &input.us);
+    module->prepare(std::move(input));
+    module->calculateObjective(1);
+    module->calculateJacobian(1);
+
+    auto output = module->output();
+
+    std::vector<double> expectedJ
+    {
+        0.00319995, -0.00583664, 0.0052654, 0.00609513, -0.0130273, -0.0197055,
+        0.00594121, -0.0225087, -0.000798031, 0.0047234, 0.00573649, 0.00852371,
+        -0.031678, -0.0117603, -0.0400357, -0.0221424, -0.00903799, 0.0016302,
+        0.0251274, -0.00673431, 0.0448595, 0.0397751, -0.0101785, 0.0714646,
+        -0.0343752, -0.0793824, 0.0108996, -0.0870476, -0.0437082, 0.0294403,
+        -1, 0, 0, -1, 0, 0,
+        0, -1, 0, 0, -1, 0,
+        0, 0, -1, 0, 0, -1,
+        0, 0, 0, -0.00333849, 0.000953458, 0.00324947,
+        0, 0, 0, 0.00128209, 0.00401972, 0.000849943,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0.00137205, -0.000933622, -0.00587537,
+        0, 0, 0, -0.00545177, -0.00138222, 0.00295494,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        -0.002673, 0.00251903, -0.00165742, 0, 0, 0,
+        -0.000205376, 0.00635746, 0.0021152, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0
+    }; // Col-major
+
+    ASSERT_EQ(expectedJ.size(), output.jacobian.size());
+    for (int i = 0; i < expectedJ.size(); ++i)
+        EXPECT_NEAR(expectedJ[i], output.jacobian[i], 0.000001);
 }
