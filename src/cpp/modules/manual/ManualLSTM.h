5177533d203501f1ed13ec77013d3588d3129b91
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-07-02 13:13:27 +0300

    Stubs for manual lstm.

---
+++ src/cpp/modules/manual/ManualLSTM.h
@@ -0,0 +1,23 @@
+// ManualLSTM.h - Contains declarations of LSTM tester functions
+#pragma once
+
+#include "../../shared/ITest.h"
+#include "../../shared/LSTMData.h"
+
+#include <vector>
+
+class ManualLSTM : public ITest<LSTMInput, LSTMOutput> {
+private:
+    LSTMInput input;
+    LSTMOutput output;
+
+public:
+    // This function must be called before any other function.
+    virtual void prepare(LSTMInput&& input) override;
+
+    virtual void calculateObjective(int times) override;
+    virtual void calculateJacobian(int times) override;
+    virtual LSTMOutput output() override;
+
+    ~ManualLSTM() {}
+};
