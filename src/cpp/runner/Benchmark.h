f07df4aa87b0d5560998de79db09e99048757d9d
Alexander Novikov <b-alnovi@microsoft.com>
2019-07-02 14:02:18 +0300

    special value in the case of impossibility of minimum measurable time reaching

--- src/cpp/runner/Benchmark.h
+++ src/cpp/runner/Benchmark.h
@@ -28,7 +28,8 @@ using test_member_function = void (ITest<Input, Output>::*) (int);
 
 //This function calls a member of ITest class passing 1 int argument.
 template <class Input, class Output>
-void call_member_function(std::unique_ptr<ITest<Input, Output>>& ptr_to_object, const test_member_function<Input, Output> ptr_to_member, const int arg1) {
+void call_member_function(std::unique_ptr<ITest<Input, Output>>& ptr_to_object,
+                          const test_member_function<Input, Output> ptr_to_member, const int arg1) {
     (*(ptr_to_object).*(ptr_to_member))(arg1);
 }
 
@@ -37,16 +38,19 @@ void call_member_function(std::unique_ptr<ITest<Input, Output>>& ptr_to_object, 
 template<class Input, class Output>
 unique_ptr<ITest<Input, Output>> get_test(const ModuleLoader& module_loader) = delete;
 
+static constexpr auto measurable_time_not_achieved{ -1 };
+
 template<class Input, class Output>
 auto find_repeats_for_minimum_measurable_time(const duration<double> minimum_measurable_time,
-    std::unique_ptr<ITest<Input, Output>>& test,
-    const test_member_function<Input, Output> func)
+                                              std::unique_ptr<ITest<Input, Output>>& test,
+                                              const test_member_function<Input, Output> func)
 {
     auto total_time = duration<double>(0s);
     auto min_sample = duration<double>(std::numeric_limits<double>::max());
 
     auto repeats = 1;
-    while (repeats < (1 << 30))
+    constexpr auto max_possible_power_of_two = (std::numeric_limits<decltype(repeats)>::max() >> 1) + 1;
+    do
     {
         auto t1 = high_resolution_clock::now();
         call_member_function(test, func, repeats);
@@ -60,18 +64,31 @@ auto find_repeats_for_minimum_measurable_time(const duration<double> minimum_mea
             total_time += current_run_time;
             break;
         }
+        //The next iteration will overflow a loop counter that's why we recognize that we cannot reach the minimum measurable time.
+        if (repeats == max_possible_power_of_two)
+        {
+            repeats = measurable_time_not_achieved;
+            break;
+        }
         repeats *= 2;
-    }
+    } while (repeats <= max_possible_power_of_two);
 
     struct result { int repeats; duration<double> sample; duration<double> total_time; };
-    return result { repeats, min_sample, total_time };
+    return result{ repeats, min_sample, total_time };
 }
 
 //Measures time according to the documentation.
 template<class Input, class Output>
-duration<double> measure_shortest_time(const duration<double> minimum_measurable_time, const int nruns, const duration<double> time_limit, std::unique_ptr<ITest<Input, Output>>& test, const test_member_function<Input, Output> func)
+duration<double> measure_shortest_time(const duration<double> minimum_measurable_time, const int nruns,
+                                       const duration<double> time_limit, std::unique_ptr<ITest<Input, Output>>& test,
+                                       const test_member_function<Input, Output> func)
 {
     auto find_repeats_result = find_repeats_for_minimum_measurable_time(minimum_measurable_time, test, func);
+
+    if (find_repeats_result == measurable_time_not_achieved)
+    {
+        throw exception("It was not possible to reach the number of repeats sufficient to achieve the minimum measurable time.");
+    }
 
     auto repeats = find_repeats_result.repeats;
     auto min_sample = find_repeats_result.sample;
@@ -100,7 +117,7 @@ std::string filepath_to_basename(const std::string& filepath);
 //Performs the entire benchmark process according to the documentation
 template<class Input, class Output>
 void run_benchmark(const char* const module_path, const std::string& input_filepath, const std::string& output_prefix, const duration<double> minimum_measurable_time, const int nruns_F, const int nruns_J,
-    const duration<double> time_limit, const bool replicate_point) {
+                   const duration<double> time_limit, const bool replicate_point) {
 
     const ModuleLoader module_loader(module_path);
     auto test = get_test<Input, Output>(module_loader);
