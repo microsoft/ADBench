b76429a25ae0ce151dbb7e0e38586bbc36038ac6
Alexander Novikov <b-alnovi@microsoft.com>
2019-07-03 16:09:43 +0300

    cpp runner lstm support

---
+++ src/cpp/runner/LSTMBenchmark.cpp
@@ -0,0 +1,27 @@
+#include "LSTMBenchmark.h"
+
+template <>
+LSTMInput read_input_data<LSTMInput>(const std::string& input_file, const bool replicate_point)
+{
+    LSTMInput input;
+
+    // Read instance
+    read_lstm_instance(input_file, &input.l, &input.c, &input.b, input.main_params, input.extra_params, input.state,
+                       input.sequence);
+
+    return input;
+}
+
+template <>
+unique_ptr<ITest<LSTMInput, LSTMOutput>> get_test<LSTMInput, LSTMOutput>(const ModuleLoader& module_loader)
+{
+    return module_loader.get_lstm_test();
+}
+
+template <>
+void save_output_to_file<LSTMOutput>(const LSTMOutput& output, const string& output_prefix,
+                                     const string& input_basename, const string& module_basename)
+{
+    save_value_to_file(objective_file_name(output_prefix, input_basename, module_basename), output.objective);
+    save_vector_to_file(jacobian_file_name(output_prefix, input_basename, module_basename), output.gradient);
+}
