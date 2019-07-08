f2ece491ccdc6be7805915a674b025481ee624cb
Alexander Novikov <b-alnovi@microsoft.com>
2019-07-08 17:07:58 +0300

    Similar binary names for all platforms

--- test/cpp/modules/manual/BaTests.cpp
+++ test/cpp/modules/manual/BaTests.cpp
@@ -3,22 +3,14 @@
 #include "../../../../src/cpp/shared/utils.h"
 
 TEST(ManualTests, Ba_Load) {
-#ifdef _DEBUG
-    ModuleLoader moduleLoader("../../../../src/cpp/modules/manual/Manuald.dll");
-#else
     ModuleLoader moduleLoader("../../../../src/cpp/modules/manual/Manual.dll");
-#endif
     auto test = moduleLoader.get_ba_test();
     ASSERT_NE(test, nullptr);
 }
 
 TEST(ManualTests, Ba_TestProcess)
 {
-#ifdef _DEBUG
-    ModuleLoader moduleLoader("../../../../src/cpp/modules/manual/Manuald.dll");
-#else
     ModuleLoader moduleLoader("../../../../src/cpp/modules/manual/Manual.dll");
-#endif
     auto module = moduleLoader.get_ba_test();
     ASSERT_NE(module, nullptr);
     BAInput input;
