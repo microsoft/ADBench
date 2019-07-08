482e7bb8e1950ddf85a6ca2b5a73cf95b8873cdb
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-07-08 13:50:50 +0300

    Studying existing lstm_d code.

--- src/cpp/modules/manual/lstm_d.h
+++ src/cpp/modules/manual/lstm_d.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include <vector>
+
+void lstm_objective_d(int l, int c, int b,
+    const double* const main_params, const double* const extra_params,
+    std::vector<double> state, const double* const sequence,
+    double* loss, double* J);
