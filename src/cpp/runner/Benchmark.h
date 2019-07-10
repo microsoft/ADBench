3a2cc298c49a1d25bd753cafb8bcbbae78694524
Alexander Novikov <b-alnovi@microsoft.com>
2019-07-10 14:58:26 +0300

    refereces to itest instead pointers

--- src/cpp/runner/Benchmark.h
+++ src/cpp/runner/Benchmark.h
@@ -28,9 +28,9 @@ using test_member_function = void (ITest<Input, Output>::*) (int);
 
 //This function calls a member of ITest class passing 1 int argument.
 template <class Input, class Output>
-void call_member_function(const std::unique_ptr<ITest<Input, Output>>& ptr_to_object,
+void call_member_function(ITest<Input, Output>& ref_to_object,
                           const test_member_function<Input, Output> ptr_to_member, const int arg1) {
-    (*(ptr_to_object).*(ptr_to_member))(arg1);
+    (ref_to_object.*(ptr_to_member))(arg1);
 }
 
 //Makes a request to the ModuleLoader to get a test of a desired type.
@@ -42,7 +42,7 @@ static constexpr auto measurable_time_not_achieved{ -1 };
 
 template<class Input, class Output>
 auto find_repeats_for_minimum_measurable_time(const duration<double> minimum_measurable_time,
-                                              const std::unique_ptr<ITest<Input, Output>>& test,
+                                              ITest<Input, Output>& test,
                                               const test_member_function<Input, Output> func)
 {
     auto total_time = duration<double>(0s);
@@ -80,7 +80,7 @@ auto find_repeats_for_minimum_measurable_time(const duration<double> minimum_mea
 //Measures time according to the documentation.
 template<class Input, class Output>
 duration<double> measure_shortest_time(const duration<double> minimum_measurable_time, const int nruns,
-                                       const duration<double> time_limit, std::unique_ptr<ITest<Input, Output>>& test,
+                                       const duration<double> time_limit, ITest<Input, Output>& test,
                                        const test_member_function<Input, Output> func)
 {
     auto find_repeats_result = find_repeats_for_minimum_measurable_time(minimum_measurable_time, test, func);
@@ -94,6 +94,7 @@ duration<double> measure_shortest_time(const duration<double> minimum_measurable
     auto min_sample = find_repeats_result.sample;
     auto total_time = find_repeats_result.total_time;
 
+    // "run" begins from 1 because a first run already done by "find_repeats_for_minimum_measurable_time" function
     for (auto run = 1; (run < nruns) && (total_time < time_limit); run++)
     {
         auto t1 = high_resolution_clock::now();
@@ -124,10 +125,10 @@ void run_benchmark(const char* const module_path, const std::string& input_filep
     test->prepare(std::move(inputs));
 
     const auto objective_time =
-        measure_shortest_time(minimum_measurable_time, nruns_F, time_limit, test, &ITest<Input, Output>::calculateObjective);
+        measure_shortest_time(minimum_measurable_time, nruns_F, time_limit, *test, &ITest<Input, Output>::calculateObjective);
 
     const auto derivative_time =
-        measure_shortest_time(minimum_measurable_time, nruns_J, time_limit, test, &ITest<Input, Output>::calculateJacobian);
+        measure_shortest_time(minimum_measurable_time, nruns_J, time_limit, *test, &ITest<Input, Output>::calculateJacobian);
 
     const auto output = test->output();
 
