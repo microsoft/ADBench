ba38d32208867051d33ecf4ed5e8a99e261763f1
Alexander Novikov <b-alnovi@microsoft.com>
2019-07-11 12:24:06 +0300

    corrected exported dll functions definition

--- src/cpp/shared/ITest.h
+++ src/cpp/shared/ITest.h
@@ -1,10 +1,19 @@
 #pragma once 
 
-#if __GNUC__ >= 4
-#define DLL_PUBLIC __attribute__ ((visibility("default")))
+#if defined _WIN32 || defined __CYGWIN__
+    #ifdef __GNUC__
+        #define DLL_PUBLIC __attribute__ ((dllexport))
+    #else
+        #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
 #else
-#define DLL_PUBLIC __declspec(dllexport)
+  #if __GNUC__ >= 4
+    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+  #else
+    #define DLL_PUBLIC
+  #endif
 #endif
+
 #include <ostream>
 
 template <typename Input, typename Output>
