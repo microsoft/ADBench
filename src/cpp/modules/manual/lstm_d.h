5eb51e0ca743cc7fa66226a93fb921882d193f82
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-07-16 12:30:00 +0300

    lstm_d: some cleanup and comments.

--- src/cpp/modules/manual/lstm_d.h
+++ src/cpp/modules/manual/lstm_d.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+// Manual gradient of lstm_objective loss (output)
+// with relation to main_params and extra_params (inputs)
 void lstm_objective_d(int l, int c, int b,
     const double* const main_params, const double* const extra_params,
     std::vector<double> state, const double* const sequence,
