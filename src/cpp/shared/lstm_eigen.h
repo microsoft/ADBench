98d52ca247a327d5cb435d0caf3a2a64fff3e5a7
Ilia Egorov <egorovIlyak@yandex.ru>
2019-07-25 17:39:47 +0300

    lstm eigen entry point

--- src/cpp/shared/lstm_eigen.h
+++ src/cpp/shared/lstm_eigen.h
@@ -5,45 +5,22 @@
 #include <cmath>
 #include <string>
 
-#include "Eigen/Dense"
-
-//using Eigen::Map;
-
 // UTILS
 
+// Sigmoid on scalar
 template<typename T>
-using ArrayX = Eigen::Array<T, 1, -1>;
-
-//// Sigmoid on scalar
-//template<typename T>
-//T sigmoid(T x) {
-//    return 1 / (1 + std::exp(-x));
-//}
-template<typename T>
-const ArrayX<T>& sigmoid(const ArrayX<T>& x) {
-    //return 1 / (1 + std::exp(-x));
-    return inverse(exp(-x) + 1);
+T sigmoid(T x) {
+    return 1 / (1 + std::exp(-x));
 }
 
-//// log(sum(exp(x), 2))
-//template<typename T>
-//T logsumexp(const T* vect, int sz) {
-//    T sum = 0.0;
-//    for (int i = 0; i < sz; ++i)
-//        sum += std::exp(vect[i]);
-//    sum += 2;
-//    return log(sum);
-//}
+// log(sum(exp(x), 2))
 template<typename T>
-const ArrayX<T>& logsumexp(const ArrayX<T>& vect) {
-    ArrayX<T> sum;
-
-    return log(exp(vect).sum() + 2);
-
-    /*for (int i = 0; i < sz; ++i)
+T logsumexp(const T* vect, int sz) {
+    T sum = 0.0;
+    for (int i = 0; i < sz; ++i)
         sum += std::exp(vect[i]);
     sum += 2;
-    return log(sum);*/
+    return log(sum);
 }
 
 // log(sum(exp(x), 2))
@@ -64,14 +41,10 @@ T logsumexp_store_temps(const T* vect, int sz) {
 template<typename T>
 struct WeightOrBias
 {
-    //const T* forget;
-    //const T* ingate;
-    //const T* outgate;
-    //const T* change;
-    ArrayX<T> forget;
-    ArrayX<T> ingate;
-    ArrayX<T> outgate;
-    ArrayX<T> change;
+    const T* forget;
+    const T* ingate;
+    const T* outgate;
+    const T* change;
 
     WeightOrBias(const T* params, int hsize) :
         forget(params),
@@ -111,12 +84,9 @@ struct MainParams
 template<typename T>
 struct ExtraParams
 {
-    //const T* in_weight;
-    //const T* out_weight;
-    //const T* out_bias;
-    ArrayX<T> in_weight;
-    ArrayX<T> out_weight;
-    ArrayX<T> out_bias;
+    const T* in_weight;
+    const T* out_weight;
+    const T* out_bias;
 
     ExtraParams(const T* params, int hsize) :
         in_weight(params),
@@ -128,22 +98,14 @@ struct ExtraParams
 template<typename T>
 struct InputSequence
 {
-    //std::vector<const T*> sequence;
-    std::vector<ArrayX<T>> sequence;
-    //T* -> ArrayX<T>
-    //ArrayX<const T*> sequence;
-    //ArrayX<T> sequence;
+    std::vector<const T*> sequence;
 
     InputSequence(const T* input_sequence, int char_bits, int char_count)
     {
         sequence.reserve(char_count);
-        //sequence.resize(char_count);
         for (int i = 0; i < char_count; ++i)
         {
-            //sequence.push_back(&input_sequence[char_bits * i]);
-            sequence.push_back(Eigen::Map<ArrayX<T>>(&input_sequence[char_bits * i], 1, char_bits)); // ERROR some convertation problem here
-            //Map<MatrixXd>(lightmatrix.data_, lightmatrix.nrows_, lightmatrix.ncols_)
-            //sequence[i] = &input_sequence[char_bits * i]; // DANGER may rewrite some data existing in sequence.
+            sequence.push_back(&input_sequence[char_bits * i]);
         }
     }
 };
@@ -151,10 +113,8 @@ struct InputSequence
 template<typename T>
 struct LayerState
 {
-    //T* hidden;
-    //T* cell;
-    ArrayX<T> hidden;
-    ArrayX<T> cell;
+    T* hidden;
+    T* cell;
 
     LayerState(T* layer_state, int hsize) :
         hidden(layer_state),
@@ -184,29 +144,18 @@ template<typename T>
 void lstm_model(int hsize,
     const LayerParams<T>& params,
     LayerState<T>& state,
-    const ArrayX<T>& input)
-//void lstm_model(int hsize,
-//    const LayerParams<T>& params,
-//    LayerState<T>& state,
-//    const T* input)
+    const T* input)
 {
-    //for (int i = 0; i < hsize; ++i) {
-    //    // gates for i-th cell/hidden
-    //    T forget = sigmoid(input[i] * params.weight.forget[i] + params.bias.forget[i]);
-    //    T ingate = sigmoid(state.hidden[i] * params.weight.ingate[i] + params.bias.ingate[i]);
-    //    T outgate = sigmoid(input[i] * params.weight.outgate[i] + params.bias.outgate[i]);
-    //    T change = tanh(state.hidden[i] * params.weight.change[i] + params.bias.change[i]);
+    for (int i = 0; i < hsize; ++i) {
+        // gates for i-th cell/hidden
+        T forget = sigmoid(input[i] * params.weight.forget[i] + params.bias.forget[i]);
+        T ingate = sigmoid(state.hidden[i] * params.weight.ingate[i] + params.bias.ingate[i]);
+        T outgate = sigmoid(input[i] * params.weight.outgate[i] + params.bias.outgate[i]);
+        T change = tanh(state.hidden[i] * params.weight.change[i] + params.bias.change[i]);
 
-    //    state.cell[i] = state.cell[i] * forget + ingate * change;
-    //    state.hidden[i] = outgate * tanh(state.cell[i]);
-    //}
-    ArrayX<T> forget = sigmoid(input * params.weight.forget + params.bias.forget);
-    ArrayX<T> ingate = sigmoid(state.hidden * params.weight.ingate + params.bias.ingate);
-    ArrayX<T> outgate = sigmoid(input * params.weight.outgate + params.bias.outgate);
-    ArrayX<T> change = tanh(state.hidden * params.weight.change + params.bias.change);
-
-    state.cell = state.cell * forget + ingate * change;
-    state.hidden = outgate * tanh(state.cell);
+        state.cell[i] = state.cell[i] * forget + ingate * change;
+        state.hidden[i] = outgate * tanh(state.cell[i]);
+    }
 }
     
 // Predict LSTM output given an input
@@ -214,18 +163,12 @@ template<typename T>
 void lstm_predict(int l, int b,
     const MainParams<T>& main_params, const ExtraParams<T>& extra_params,
     State<T>& state,
-    const ArrayX<T>& input, const ArrayX<T>& output)
-//void lstm_predict(int l, int b,
-//    const MainParams<T>& main_params, const ExtraParams<T>& extra_params,
-//    State<T>& state,
-//    const T* input, T* output)
+    const T* input, T* output)
 {
-    /*for (int i = 0; i < b; ++i)
-        output[i] = input[i] * extra_params.in_weight[i];*/
-    output = input * (ArrayX<T>)extra_params.in_weight; // DANGER types may be incompatible
+    for (int i = 0; i < b; ++i)
+        output[i] = input[i] * extra_params.in_weight[i];
 
-    //T* layer_output = output;
-    ArrayX<T> layer_output = output;
+    T* layer_output = output;
 
     for (int i = 0; i < l; ++i)
     {
@@ -233,9 +176,8 @@ void lstm_predict(int l, int b,
         layer_output = state.layer_state[i].hidden;
     }
 
-    /*for (int i = 0; i < b; ++i)
-        output[i] = layer_output[i] * extra_params.out_weight[i] + extra_params.out_bias[i];*/
-    output = layer_output * extra_params.out_weight + extra_params.out_bias;
+    for (int i = 0; i < b; ++i)
+        output[i] = layer_output[i] * extra_params.out_weight[i] + extra_params.out_bias[i];
 }
     
 
@@ -252,30 +194,18 @@ void lstm_objective(int l, int c, int b,
     ExtraParams<T> extra_params_wrap(extra_params, b);
     State<T> state_wrap(state.data(), b, l);
     InputSequence<T> sequence_wrap(sequence, b, c);
-    ArrayX<T> ypred(b), ynorm(b);
+    std::vector<T> ypred(b), ynorm(b);
     for (int t = 0; t < c - 1; ++t)
     {
-        //ypred, new_state = predict(main_params, extra_params, all_states[t], _input)
-        //all_states.append(new_state)
-        //ynorm = ypred - torch.log(sum(torch.exp(ypred), 2))
-        //ygold = sequence[t + 1]
-        //total += sum(ygold * ynorm)
-        //count += ygold.shape[0]
-        //_input = ygold
+        lstm_predict(l, b, main_params_wrap, extra_params_wrap, state_wrap, sequence_wrap.sequence[t], ypred.data());
 
-        //lstm_predict(l, b, main_params_wrap, extra_params_wrap, state_wrap, sequence_wrap.sequence[t], ypred.data());
-        lstm_predict(l, b, main_params_wrap, extra_params_wrap, state_wrap, sequence_wrap.sequence[t], ypred);
-
-        /*T lse = logsumexp(ypred.data(), b);
+        T lse = logsumexp(ypred.data(), b);
         for (int i = 0; i < b; ++i)
-            ynorm[i] = ypred[i] - lse;*/
-        ynorm = ypred - logsumexp(ypred);//log(sum(exp(ypred), 2))
+            ynorm[i] = ypred[i] - lse;
 
-        //const T* ygold = sequence_wrap.sequence[t + 1];
-        const ArrayX<T> ygold = sequence_wrap.sequence[t + 1];
-        total += (ygold * ynorm).sum();
-        /*for (int i = 0; i < b; ++i)
-            total += ygold[i] * ynorm[i];*/
+        const T* ygold = sequence_wrap.sequence[t + 1];
+        for (int i = 0; i < b; ++i)
+            total += ygold[i] * ynorm[i];
 
         count += b;
     }
