86be67e2ac596bde099ff682ca9e27cc608450f9
Ilia Egorov <egorovIlyak@yandex.ru>
2019-08-01 11:15:45 +0300

    lstm_eigen.h structures rewrited (without copying data)

--- src/cpp/modules/manualEigen/lstm_eigen_helpers.h
+++ src/cpp/modules/manualEigen/lstm_eigen_helpers.h
@@ -3,5 +3,12 @@
 #include "Eigen/Dense"
 
 using Eigen::Map;
+
 template<typename T>
 using ArrayX = Eigen::Array<T, -1, 1>;
+
+template<typename T>
+using MapX = Eigen::Map<ArrayX<T>>;
+
+template<typename T>
+using MapConstX = Eigen::Map<const ArrayX<T>>;
