c1367fa0807ef19e3a8e74bc5478bd954e927864
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-08-02 13:13:33 +0300

    FiniteEigen module for hand tracking.

---
+++ src/cpp/modules/finiteEigen/FiniteEigenHand.cpp
@@ -0,0 +1,77 @@
+// FiniteEigenHand.cpp : Defines the exported functions for the DLL.
+#include "FiniteEigenHand.h"
+
+#include "../../shared/hand_eigen.h"
+#include "../finite/finite.h"
+
+// This function must be called before any other function.
+void FiniteEigenHand::prepare(HandInput&& input)
+{
+    copy_HandInput_to_HandEigenInput(input, this->input);
+
+    complicated = this->input.us.size() != 0;
+    int err_size = 3 * this->input.data.correspondences.size();
+    int ncols = (complicated ? 2 : 0) + this->input.theta.size();
+    result = { std::vector<double>(err_size), ncols, err_size, std::vector<double>(err_size * ncols) };
+    engine.set_max_output_size(err_size);
+    if (complicated)
+        jacobian_by_us = std::vector<double>(2 * err_size);
+}
+
+HandOutput FiniteEigenHand::output()
+{
+    return result;
+}
+
+void FiniteEigenHand::calculateObjective(int times)
+{
+    if (complicated)
+    {
+        for (int i = 0; i < times; ++i) {
+            hand_objective(input.theta.data(), input.us.data(), input.data, result.objective.data());
+        }
+    }
+    else
+    {
+        for (int i = 0; i < times; ++i) {
+            hand_objective(input.theta.data(), input.data, result.objective.data());
+        }
+    }
+}
+
+void FiniteEigenHand::calculateJacobian(int times)
+{
+    if (complicated)
+    {
+        for (int i = 0; i < times; ++i) {
+            engine.finite_differences([&](double* theta_in, double* err) {
+                hand_objective(theta_in, input.us.data(), input.data, err);
+                }, input.theta.data(), input.theta.size(), result.objective.data(), result.objective.size(), &result.jacobian.data()[6 * input.data.correspondences.size()]);
+
+            for (int j = 0; j < input.us.size() / 2; ++j) {
+                engine.finite_differences_continue([&](double* us_in, double* err) {
+                    // us_in points into the middle of _input.us.data()
+                    hand_objective(input.theta.data(), input.us.data(), input.data, err);
+                    }, &input.us.data()[j * 2], 2, result.objective.data(), result.objective.size(), jacobian_by_us.data());
+
+                for (int k = 0; k < 3; ++k) {
+                    result.jacobian[j * 3 + k] = jacobian_by_us[j * 3 + k];
+                    result.jacobian[j * 3 + k + result.objective.size()] = jacobian_by_us[j * 3 + k + result.objective.size()];
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < times; ++i) {
+            engine.finite_differences([&](double* theta_in, double* err) {
+                hand_objective(theta_in, input.data, err);
+                }, input.theta.data(), input.theta.size(), result.objective.data(), result.objective.size(), result.jacobian.data());
+        }
+    }
+}
+
+extern "C" DLL_PUBLIC ITest<HandInput, HandOutput>* GetHandTest()
+{
+    return new FiniteEigenHand();
+}
--- src/cpp/modules/manualEigen/ManualEigenHand.cpp
+++ src/cpp/modules/manualEigen/ManualEigenHand.cpp
@@ -6,44 +6,10 @@
 #include <iostream>
 #include <memory>
 
-auto convert_matrix(LightMatrix<double>& lightmatrix)
-{
-    return Map<MatrixXd>(lightmatrix.data_, lightmatrix.nrows_, lightmatrix.ncols_);
-};
-
 // This function must be called before any other function.
 void ManualEigenHand::prepare(HandInput&& input)
 {
-    _input.theta = input.theta;
-    _input.us = input.us;
-    _input.data.correspondences = input.data.correspondences;
-    _input.data.model.bone_names = input.data.model.bone_names;
-    _input.data.model.parents = input.data.model.parents;
-    _input.data.model.triangles = input.data.model.triangles;
-    _input.data.model.is_mirrored = input.data.model.is_mirrored;
-
-    // Matrix& operator=(const DenseBase<OtherDerived>& other)
-    // Copies the value of the expression other into *this with automatic resizing.
-    _input.data.points = convert_matrix(input.data.points);
-    int n_vertices = input.data.model.base_positions.cols();
-    _input.data.model.base_positions.resize(3, n_vertices);
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < n_vertices; j++)
-        {
-            _input.data.model.base_positions(i, j) = input.data.model.base_positions(i, j);
-        }
-    }
-    _input.data.model.weights = convert_matrix(input.data.model.weights);
-
-    for (int i = 0; i < input.data.model.base_relatives.size(); i++)
-    {
-        _input.data.model.base_relatives.push_back(convert_matrix(input.data.model.base_relatives[i]));
-    }
-    for (int i = 0; i < input.data.model.inverse_base_absolutes.size(); i++)
-    {
-        _input.data.model.inverse_base_absolutes.push_back(convert_matrix(input.data.model.inverse_base_absolutes[i]));
-    }
+    copy_HandInput_to_HandEigenInput(input, _input);
 
     _complicated = _input.us.size() != 0;
     int err_size = 3 * _input.data.correspondences.size();
