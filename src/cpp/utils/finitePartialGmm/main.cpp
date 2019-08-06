59d0e3226da65bf58864a35d72ba531a585d8f88
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-08-06 15:39:04 +0300

    Utility to compute partial gradiens for Gmm using finite differences.

---
+++ src/cpp/utils/finitePartialGmm/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include "../../shared/ITest.h"
+#include "../../shared/GMMData.h"
+#include "../../shared/utils.h"
+#include "../../shared/gmm.h"
+#include "../../modules/finite/finite.h"
+#include "../../runner/Filepaths.h"
+#include "../../runner/OutputSave.h"
+
+int main(const int argc, const char* argv[])
+{
+    try {
+        if (argc < 3) {
+            std::cerr << "usage: FinitePartialGmm input_filepath output_dir gradient_size_limit\n";
+            return 1;
+        }
+        const std::string input_filepath(argv[0]);
+        const std::string output_prefix(argv[1]);
+        const auto max_grad_size = std::stoi(argv[2]);
+
+        GMMInput input;
+
+        // Read instance
+        read_gmm_instance(input_filepath, &input.d, &input.k, &input.n,
+            input.alphas, input.means, input.icf, input.x, input.wishart, false);
+
+        int out_alphas = std::min(max_grad_size, input.k);
+        int out_means = std::min(max_grad_size, input.d * input.k);
+        int out_icfs = std::min(max_grad_size, (input.k * input.d * (input.d + 1)) / 2);
+        double objective;
+        std::vector<double> d_alphas(out_alphas), d_means(out_means), d_icfs(out_icfs);
+        FiniteDifferencesEngine<double> engine(1);
+
+        engine.finite_differences([&](double* alphas_in, double* err) {
+            gmm_objective(input.d, input.k, input.n, alphas_in, input.means.data(), input.icf.data(), input.x.data(), input.wishart, err);
+            }, input.alphas.data(), out_alphas, &objective, 1, d_alphas.data());
+
+        engine.finite_differences_continue([&](double* means_in, double* err) {
+            gmm_objective(input.d, input.k, input.n, input.alphas.data(), means_in, input.icf.data(), input.x.data(), input.wishart, err);
+            }, input.means.data(), out_means, &objective, 1, d_means.data());
+
+        engine.finite_differences_continue([&](double* icf_in, double* err) {
+            gmm_objective(input.d, input.k, input.n, input.alphas.data(), input.means.data(), icf_in, input.x.data(), input.wishart, err);
+            }, input.icf.data(), out_icfs, &objective, 1, d_icfs.data());
+
+        const auto input_basename = filepath_to_basename(input_filepath);
+        save_vector_to_file(jacobian_file_name(output_prefix, input_basename, "alphas"), d_alphas);
+        save_vector_to_file(jacobian_file_name(output_prefix, input_basename, "means"), d_means);
+        save_vector_to_file(jacobian_file_name(output_prefix, input_basename, "icfs"), d_icfs);
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "An exception caught: " << ex.what() << std::endl;
+    }
+    catch (const std::string& ex)
+    {
+        std::cerr << "An exception caught: " << ex << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception" << std::endl;
+    }
+}
