65272b55785e2696164448b5fdc87c095846139f
Ilia Egorov <egorovIlyak@yandex.ru>
2019-08-15 16:09:38 +0300

    some comments & lstm eigen structures code cleanup

--- src/cpp/modules/manualEigen/lstm_eigen_helpers.h
+++ src/cpp/modules/manualEigen/lstm_eigen_helpers.h
@@ -4,6 +4,7 @@
 
 using Eigen::Map;
 
+//Dynamic sized column array
 template<typename T>
 using ArrayX = Eigen::Array<T, -1, 1>;
 
@@ -16,14 +17,18 @@ using MapConstX = Eigen::Map<const ArrayX<T>>;
 template<typename T>
 using ArrayX10 = Eigen::Array<T, -1, 10>;
 
+//Map of array with X rows and 10 cols
 template<typename T>
 using MapX10 = Eigen::Map< Eigen::Array<T, -1, 10>>;
 
+//Map of array with X rows and 8 cols
 template<typename T>
 using MapX8 = Eigen::Map< Eigen::Array<T, -1, 8>>;
 
+//Map of array with X rows and 3 cols
 template<typename T>
 using MapX3 = Eigen::Map< Eigen::Array<T, -1, 3>>;
 
+//3 elements in row array map
 template<typename T>
 using MapRow3 =  Eigen::Map<Eigen::Array<T, 1, 3>>;
