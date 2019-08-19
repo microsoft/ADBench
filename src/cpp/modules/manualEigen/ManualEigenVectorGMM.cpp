89f8bcdb071ec50e0b37b870e26b0eafa1236daa
Ilia Egorov <egorovIlyak@yandex.ru>
2019-08-19 14:31:31 +0300

    manual eigen Vector added from old branch

--- src/cpp/modules/manualEigen/ManualEigenVectorGMM.cpp
+++ src/cpp/modules/manualEigen/ManualEigenVectorGMM.cpp
@@ -1,25 +1,25 @@
-// ManualEigenGMM.cpp : Defines the exported functions for the DLL.
-#include "ManualEigenGMM.h"
+// ManualEigenVectorGMM.cpp : Defines the exported functions for the DLL.
+#include "ManualEigenVectorGMM.h"
 #include "../../shared/gmm.h"
-#include "gmm_scalar_d.h"
+#include "gmm_vector_d.h"
 
 #include <iostream>
 #include <memory>
 
 // This function must be called before any other function.
-void ManualEigenGMM::prepare(GMMInput&& input)
+void ManualEigenVectorGMM::prepare(GMMInput&& input)
 {
     _input = input;
     int Jcols = (_input.k * (_input.d + 1) * (_input.d + 2)) / 2;
     _output = { 0,  std::vector<double>(Jcols) };
 }
 
-GMMOutput ManualEigenGMM::output()
+GMMOutput ManualEigenVectorGMM::output()
 {
     return _output;
 }
 
-void ManualEigenGMM::calculate_objective(int times)
+void ManualEigenVectorGMM::calculate_objective(int times)
 {
     for (int i = 0; i < times; ++i) {
         gmm_objective(_input.d, _input.k, _input.n, _input.alphas.data(), _input.means.data(),
@@ -27,7 +27,7 @@ void ManualEigenGMM::calculate_objective(int times)
     }
 }
 
-void ManualEigenGMM::calculate_jacobian(int times)
+void ManualEigenVectorGMM::calculate_jacobian(int times)
 {
     for (int i = 0; i < times; ++i) {
         gmm_objective_d(_input.d, _input.k, _input.n, _input.alphas.data(), _input.means.data(),
@@ -35,7 +35,7 @@ void ManualEigenGMM::calculate_jacobian(int times)
     }
 }
 
-extern "C" DLL_PUBLIC ITest<GMMInput, GMMOutput>* get_gmm_test()
+extern "C" __declspec(dllexport) ITest<GMMInput, GMMOutput>* __cdecl get_gmm_test()
 {
-    return new ManualEigenGMM();
+    return new ManualEigenVectorGMM();
 }
