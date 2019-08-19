c66b052a4b8b4a3d0e073b419a1b40c24679695e
Ilia Egorov <egorovIlyak@yandex.ru>
2019-08-19 14:31:31 +0300

    manual eigen Vector added from old branch

--- src/cpp/modules/manualEigen/gmm_d.h
+++ src/cpp/modules/manualEigen/gmm_d.h
@@ -2,26 +2,37 @@
 
 #include "../../shared/defs.h"
 
-// d: dim
-// k: number of gaussians
-// n: number of points
-// alphas: k logs of mixture weights (unnormalized), so
-//            weights = exp(log_alphas) / sum(exp(log_alphas))
-// means: d*k component means
-// icf: (d*(d+1)/2)*k inverse covariance factors 
-//                    every icf entry stores firstly log of diagonal and then 
-//          columnwise other entris
-//          To generate icf in MATLAB given covariance C :
-//              L = inv(chol(C, 'lower'));
-//              inv_cov_factor = [log(diag(L)); L(au_tril_indices(d, -1))]
-// wishart: wishart distribution parameters
-// x: d*n points
-// err: 1 output
-void gmm_objective_d(int d, int k, int n,
-  const double *alphas,
-  const double *means,
-  const double *icf,
-  const double *x,
-  Wishart wishart,
-  double *err,
-  double *J);
+#include "Eigen/Dense"
+#include <vector>
+
+using Eigen::ArrayXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+void Qtransposetimesx(int d,
+    const double* const Ldiag,
+    const double* const icf,
+    const double* const x,
+    double* Ltransposex);
+
+void compute_q_inner_term(int d,
+    const double* const Ldiag,
+    const double* const xcentered,
+    const double* const Lxcentered,
+    double* logLdiag_d);
+
+void compute_L_inner_term(int d,
+    const double* const xcentered,
+    const double* const Lxcentered,
+    double* L_d);
+
+double logsumexp_d(int n, const double* const x, double* logsumexp_partial_d);
+
+double logsumexp_d(const ArrayXd& x, ArrayXd& logsumexp_partial_d);
+
+double log_wishart_prior_d(int p, int k,
+    Wishart wishart,
+    const ArrayXd& sum_qs,
+    const vector<MatrixXd>& Qs,
+    const double* icf,
+    double* J);
