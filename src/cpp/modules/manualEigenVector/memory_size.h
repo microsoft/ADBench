00adde01b76d1f15baa28526313f975c00c07a13
Ilia Egorov <egorovIlyak@yandex.ru>
2019-08-21 16:50:13 +0300

    size_t to unsigned long long int

--- src/cpp/modules/manualEigenVector/memory_size.h
+++ src/cpp/modules/manualEigenVector/memory_size.h
@@ -1,16 +1,4 @@
 #pragma once
 
-#if defined(_WIN32)
-#include <windows.h>
-
-#elif defined(__linux__)
-#include <cstddef>
-#include <sys/sysinfo.h>
-using namespace std;
-
-#else
-#error "Cannot define size of memory for current OS."
-#endif
-
 // Returns the size of physical memory (RAM) in bytes.
-size_t get_memory_size();
+unsigned long long int get_memory_size();
