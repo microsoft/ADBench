74861d6b71d2d3a87828f1a2ac4a02c170f2a117
Ilia Egorov <egorovIlyak@yandex.ru>
2019-08-21 15:25:27 +0300

    added memory_size check to prevent system deadlock

---
+++ src/cpp/modules/manualEigenVector/memory_size.cpp
@@ -0,0 +1,50 @@
+#include "memory_size.h"
+
+#if defined(_WIN32)
+#include <windows.h>
+
+#elif defined(__linux__)
+#include <cstddef>
+#include <sys/sysinfo.h>
+
+#else
+#error "Cannot define size of memory for current OS."
+#endif
+
+size_t get_memory_size()
+{
+#if defined(_WIN32)
+    MEMORYSTATUSEX status;
+    status.dwLength = sizeof(status);
+    GlobalMemoryStatusEx(&status);
+    return (size_t)status.ullTotalPhys;
+#elif defined(__linux__)
+    struct sysinfo sys_info;
+    size_t total_ram = 0;
+    if (sysinfo(&sys_info) != -1)
+        total_ram = ((size_t)sys_info.totalram * sys_info.mem_unit);
+    return total_ram;
+#else
+    // Unknown OS
+    return 0L;
+#endif
+}
+
+    //FILE* meminfo = fopen("/proc/meminfo", "r");
+    //if (meminfo == NULL)
+    //    // handle error
+    //
+    //char line[256];
+    //while (fgets(line, sizeof(line), meminfo))
+    //{
+    //    int ram;
+    //    if (sscanf(line, "MemTotal: %d kB", &ram) == 1)
+    //    {
+    //        fclose(meminfo);
+    //        return ram;
+    //    }
+    //}
+    //
+    //// If we got here, then we couldn't find the proper line in the meminfo file
+    //fclose(meminfo);
+    //return 0;
