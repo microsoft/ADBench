59de431ec2e2843e34d1d453b2723651086d8209
Mikhail Nikolaev (Business Guest) <b-miniko@microsoft.com>
2019-08-22 04:17:43 +0300

    Tapenade BA derivateve is added

---
+++ src/cpp/modules/tapenade/ba.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdlib>
+#include <cmath>
+#include <cfloat>
+
+#include "../../shared/defs.h"
+
+// n number of cameras
+// m number of points
+// p number of observations
+// cams: 11*n cameras in format [r1 r2 r3 C1 C2 C3 f u0 v0 k1 k2]
+//            r1, r2, r3 are angle - axis rotation parameters(Rodrigues)
+//            [C1 C2 C3]' is the camera center
+//            f is the focal length in pixels
+//            [u0 v0]' is the principal point
+//            k1, k2 are radial distortion parameters
+// X: 3*m points
+// obs: 2*p observations (pairs cameraIdx, pointIdx)
+// feats: 2*p features (x,y coordinates corresponding to observations)
+// reproj_err: 2*p errors of observations
+// w_err: p weight "error" terms
+void ba_objective(
+    int n,
+    int m,
+    int p,
+    const double* const cams,
+    const double* const X,
+    const double* const w,
+    const int* const obs,
+    const double* const feats,
+    double* reproj_err,
+    double* w_err
+);
