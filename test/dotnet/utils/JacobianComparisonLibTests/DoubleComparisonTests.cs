f28cdf0281c582411035a71a53d2d72c336a6bbf
Dmitry Kats <ratkillerx@hotmail.com>
2019-08-23 17:36:03 +0300

    Dotnet puts intermediate output near binaries. Started tests for JacobianComparisonLib.

---
+++ test/dotnet/utils/JacobianComparisonLibTests/DoubleComparisonTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using JacobianComparisonLib;
+
+namespace JacobianComparisonLibTests
+{
+    public class DoubleComparisonTests
+    {
+        [Fact]
+        public void InfinityEquality()
+        {
+            var diff = JacobianComparison.Difference(double.PositiveInfinity, double.PositiveInfinity);
+            Assert.Equal(0.0, diff);
+            diff = JacobianComparison.Difference(double.NegativeInfinity, double.NegativeInfinity);
+            Assert.Equal(0.0, diff);
+        }
+    }
+}
