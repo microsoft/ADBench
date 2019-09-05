5f54def95b70dd99446026848351ac6364f35c2e
Alexander Novikov <b-alnovi@microsoft.com>
2019-09-05 12:48:28 +0300

    dotnet runner sketch

---
+++ src/dotnet/runner/DotnetRunner/ITest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetRunner
+{
+    interface ITest<TInput, TOutput>
+    {
+        // This function must be called before any other function.
+        void Prepare(TInput input);
+        // calculate function
+        void CalculateObjective(int times);
+        void CalculateJacobian(int times);
+        TOutput Output();
+    }
+}
