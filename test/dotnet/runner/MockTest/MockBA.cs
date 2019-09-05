f652fb78b7be124d53a344b920ce8150dca839fc
Alexander Novikov <b-alnovi@microsoft.com>
2019-09-05 16:12:06 +0300

    dotnet module loader

---
+++ test/dotnet/runner/MockTest/MockBA.cs
@@ -0,0 +1,31 @@
+﻿using DotnetRunner;
+using DotnetRunner.Data;
+using System;
+using System.Composition;
+
+namespace MockTest
+{
+    [Export(typeof(ITest<BAInput, BAOutput>))]
+    public class MockBA : ITest<BAInput, BAOutput>
+    {
+        public void Prepare(BAInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateJacobian(int times)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateObjective(int times)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BAOutput Output()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/dotnet/runner/MockTest/MockHand.cs
@@ -0,0 +1,31 @@
+﻿using DotnetRunner;
+using DotnetRunner.Data;
+using System;
+using System.Composition;
+
+namespace MockTest
+{
+    [Export(typeof(ITest<HandInput, HandOutput>))]
+    public class MockHand : ITest<HandInput, HandOutput>
+    {
+        public void Prepare(HandInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateJacobian(int times)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateObjective(int times)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HandOutput Output()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
