8cb5d3b9dd14019bbb0abdb5c72183f1de4a8d68
Alexander Novikov <b-alnovi@microsoft.com>
2019-09-06 13:30:06 +0300

    dotnet time measurement

--- test/dotnet/runner/DotnetRunnerTests/RunnerTests.cs
+++ test/dotnet/runner/DotnetRunnerTests/RunnerTests.cs
@@ -1,6 +1,5 @@
 using DotnetRunner;
 using DotnetRunner.Data;
-using MockTest;
 using Moq;
 using System;
 using System.IO;
@@ -27,18 +26,18 @@ namespace DotnetRunnerTests
             var gmmTestMock = new Mock<ITest<GMMInput, GMMOutput>>();
             var gmmTest = gmmTestMock.Object;
 
-            //Run_count guarantees total time greater than the time_limit
+            //runCount guarantees totalTime greater than the timeLimit
             var runCount = 100;
             var timeLimit = TimeSpan.FromSeconds(0.1);
             //executionTime should be more than minimumMeasurableTime 
-            //because we can expect find_repeats_for_minimum_measurable_time to call calculate_objective function only once in that case.
+            //because we can expect FindRepeatsForMinimumMeasurableTime to call CalculateObjective function only once in that case.
             var minimumMeasurableTime = TimeSpan.FromSeconds(0); ;
             var executionTime = TimeSpan.FromSeconds(0.01); ;
 
             gmmTestMock.Setup(test => test.CalculateObjective(It.IsAny<int>()))
                        .Callback((int _) => System.Threading.Thread.Sleep(executionTime));
 
-            /*measureShortestTime(minimumMeasurableTime, runCount, timeLimit, gmmTestMock, gmmTestMock.Ca);*/
+            Benchmark.MeasureShortestTime(minimumMeasurableTime, runCount, timeLimit, gmmTest.CalculateObjective);
             
             //Number of runs should be less then runCount variable because totalTime will be reached. 
             gmmTestMock.Verify(test => test.CalculateObjective(It.IsAny<int>()), Times.Between(1, (int)Math.Ceiling(timeLimit/executionTime), Range.Exclusive));
