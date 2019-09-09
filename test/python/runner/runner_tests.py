0b19b9a3c4171207559a5a2ac9b2e6bd4e70ed77
Ilia Egorov <egorovIlyak@yandex.ru>
2019-09-09 17:51:02 +0300

    python_runner runner_tests code cleanup

--- test/python/runner/runner_tests.py
+++ test/python/runner/runner_tests.py
@@ -2,7 +2,6 @@ import unittest
 import sys
 sys.path.append(sys.path[0] + ("/" if sys.path[0] else None) + "../../../src/python/")
 from runner.ModuleLoader import ModuleLoader
-# from runner.main import main as main_test
 
 class PythonRunnerTests(unittest.TestCase):
 
@@ -11,12 +10,6 @@ class PythonRunnerTests(unittest.TestCase):
         module_loader = ModuleLoader(module_path)
         test = module_loader.get_test()
         self.assertIsNotNone(test)
-
-    # def test_B(self):
-    #     self.fail("Not implemented")
-    # def test_C(self):
-    #     self.assertEqual(0,
-    #     main_test([self, "GMM", r"C:\Users\egoro\ADB/tools/PyTorch/PyTorch_gmm.py", r"C:\Users\egoro\ADB/data/gmm/1k/gmm_d2_K5.txt", r"C:\Users\egoro\ADB/tmp/Debug/gmm/1k/PyTorch/", 10, 10, 10, 2]))
 
 if __name__ == '__main__':
     unittest.main()
