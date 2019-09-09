f0ed0c83af7c2a3c287bc8d188a79925972ac8e1
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-09-09 12:05:49 +0300

    Tidied up test loading logic.

--- src/julia/runner/TestLoader.jl
+++ src/julia/runner/TestLoader.jl
@@ -19,17 +19,22 @@ get_hand_test(module_name::String)::Test{HandInput, HandOutput} = get_test("hand
 get_lstm_test(module_name::String)::Test{LSTMInput, LSTMOutput} = get_test("lstm", module_name)
 
 function get_test(test_name::String, module_name::String)
+    box_module_name = module_name * "Box"
     if !(module_name ∈ loaded_modules)
-        include_string(Box, "$module_name = Module()\n")
+        # Creating a new submodule coorespondig to the module being loaded
+        @eval(Box, $(Symbol(box_module_name)) = Module())
+        # "using" the module being loaded in the new submodule
+        @eval(Box, @eval($(Symbol(box_module_name)), using $(Symbol(module_name))))
+        push!(loaded_modules, module_name)
     end
-    usingstr = "using $module_name\n"
-    module_symbol = Meta.parse("Box.$module_name")
     get_test_function_name = "get_$(test_name)_test"
-    @eval include_string($module_symbol, $usingstr)
-    if @eval !isdefined($module_symbol, Symbol($get_test_function_name))
+    # Checking that the submodule that uses the target module has a test factory function defined
+    if @eval !isdefined($(Meta.parse("Box.$box_module_name")), Symbol($get_test_function_name))
         throw(ArgumentError("Module $module_name doesn't export a funtion $get_test_function_name."))
     end
-    @eval Base.invokelatest($(Meta.parse("Box.$module_name.$get_test_function_name")))
+    # Evaluating test factory function that we imported ("used") from the module $module_name
+    # into submodule Box.$box_module_name
+    @eval Base.invokelatest($(Meta.parse("Box.$box_module_name.$get_test_function_name")))
 end
 
 end
