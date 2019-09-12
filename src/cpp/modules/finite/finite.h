2610e469252fc93c894fd98c4f9764e4e5d9198e
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-09-12 17:10:44 +0300

    Finite differences determine step with a user-specified callback.

--- src/cpp/modules/finite/finite.h
+++ src/cpp/modules/finite/finite.h
@@ -3,13 +3,35 @@
 #include <vector>
 #include <functional>
 #include <algorithm>
+#include <iostream>
 
 const double FINITE_DIFFERENCES_DEFAULT_EPSILON = std::cbrt(std::numeric_limits<double>::epsilon());
 const double FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_RADIUS = 1;
 const double FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_CHARACTERISTIC_SCALE = 1;
 
+/// <summary>Generates a function that returns a value proportional to some characteristic scale
+///     when the argument lies within a neighborhood of zero or a value proportional to the
+///     argument itself otherwise.</summary>
+/// <param name="epsilon">Coefficient by which the value of the input variable is multiplied
+///     to determine the difference to use in finite differentiation when the absolute value
+///     of the said variable is greater or equal to zero_neighborhood_radius</param>
+/// <param name="zero_neighborhood_radius">Radius of the neighborhood of zero where the difference
+///     used in finite differentiation should not be proportional to the input</param>
+/// <param name="zero_neighborhood_characteristic_scale">Value, which when multiplied by epsilon
+///     gives the difference to use in finite differentiation when the absolute value
+///     of the said variable is lesser than zero_neighborhood_radius</param>
+template<typename T>
+std::function<T(T)> finite_differences_default_step_function(T epsilon = FINITE_DIFFERENCES_DEFAULT_EPSILON, T zero_neighborhood_radius = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_RADIUS, T zero_neighborhood_characteristic_scale = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_CHARACTERISTIC_SCALE) {
+    return [epsilon, zero_neighborhood_radius, zero_neighborhood_characteristic_scale](T x) {
+        T absx = std::abs(x);
+        return absx >= zero_neighborhood_radius ? absx * epsilon : zero_neighborhood_characteristic_scale * epsilon;
+    };
+}
+
 // Engine for arrpoximate differentiation using finite differences.
 // Shares temporary memory buffer between calls.
+// Encapsulates a function that chooses the step for finite differences
+// based on the input value.
 template<typename T>
 class FiniteDifferencesEngine
 {
@@ -43,12 +65,15 @@ private:
 
 public:
     // max_output_size - maximum size of the ouputs of the functions
-    // this engine will be able to approximately differentiate
-    FiniteDifferencesEngine(int max_output_size): max_output_size(max_output_size), tmp_output_f(max_output_size), tmp_output_b(max_output_size)
+    // this engine will be able to approximately differentiate.
+    // step_function - a function that chooses the step for finite differences
+    // based on the input value. Defaults to finite_differences_default_step_function().
+    // For fixed step use [](T x){ return STEP_VALUE; }
+    FiniteDifferencesEngine(int max_output_size = 0, std::function<T(T)> step_function = finite_differences_default_step_function<T>()): max_output_size(max_output_size), tmp_output_f(max_output_size), tmp_output_b(max_output_size), step(step_function)
     {}
 
-    FiniteDifferencesEngine() : max_output_size(0), tmp_output_f(0), tmp_output_b(0)
-    {}
+    // Computes finite differences step for given argument.
+    std::function<T(T)> step;
 
     // sets max_output_size - maximum size of the ouputs of the functions
     // this engine is be able to approximately differentiate
@@ -68,7 +93,9 @@ public:
 
     // FINITE DIFFERENTIATION FUNCTION
 
-    /// <summary>Approximately differentiate a function using finite differences</summary>
+    /// <summary>Approximately differentiate a function using finite differences
+    ///     Step for differentiation is determined separately for every input
+    ///     by calling this.step(input)</summary>
     /// <param name="func">Function to be differentiated.
     ///		Should accept 2 pointers as arguments (one input, one output).
     ///		Each output will be differentiated with respect to each input.</param>
@@ -78,31 +105,22 @@ public:
     /// <param name="result">Pointer to where resultant Jacobian should go.
     ///		Will be stored as a vector(input_size * output_size).
     ///		Will store in format foreach (input) { foreach (output) {} }</param>
-    /// <param name="epsilon">Coefficient by which the value of the input variable is multiplied
-    ///     to determine the difference to use in finite differentiation when the absolute value
-    ///     of the said variable is greater or equal to zero_neighborhood_radius</param>
-    /// <param name="zero_neighborhood_radius">Radius of the neighborhood of zero where the difference
-    ///     used in finite differentiation should not be proportional to the input</param>
-    /// <param name="zero_neighborhood_characteristic_scale">Value, which when multiplied by epsilon
-    ///     gives the difference to use in finite differentiation when the absolute value
-    ///     of the said variable is lesser than zero_neighborhood_radius</param>
     void finite_differences(std::function<void(T*, T*)> func,
         T* input, int input_size, int output_size,
-        T* result, T epsilon = FINITE_DIFFERENCES_DEFAULT_EPSILON,
-        T zero_neighborhood_radius = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_RADIUS,
-        T zero_neighborhood_characteristic_scale = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_CHARACTERISTIC_SCALE)
+        T* result)
     {
         volatile T tmp_f, tmp_b;
         for (int i = 0; i < input_size; i++)
         {
             T originalInput = input[i];
-            T absInput = std::abs(originalInput);
-            T delta = absInput >= zero_neighborhood_radius ? absInput * epsilon : zero_neighborhood_characteristic_scale * epsilon;
+            T delta = step(originalInput);
             tmp_b = originalInput - delta;
             T dx = delta * 2;
             tmp_f = tmp_b + dx;
             // adjusting dx so that (tmp_b + dx) - tmp_b == delta
             dx = tmp_f - tmp_b;
+            if (dx < delta * 0.02)
+                std::cerr << "WARNING: Finite difference step " << delta << " seems incompatible with the argument " << originalInput << std::endl;
 
             input[i] = tmp_f;
             func(input, tmp_output_f.data());
