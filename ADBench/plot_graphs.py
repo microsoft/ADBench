5223d488f1bbcbdd5c3d6835fe4301e26850b16b
Tom Ellis <tom.ellis@microsoft.com>
2019-09-16 17:45:29 +0100

    Add get_non_infinite_y_list function

--- ADBench/plot_graphs.py
+++ ADBench/plot_graphs.py
@@ -139,7 +139,7 @@ for (figure_idx, (graph, function_type)) in enumerate(all_graphs, start=1):
     graph_files = [path for path in all_files if path[:len(graph)] == graph]
 
     sorted_vals_by_tool = sorted(vals_by_tool(objective, graph_files),
-                                 key=lambda t: -safe_mean([y for y in t[2] if y != float("inf")]))
+                                 key=lambda t: -safe_mean(utils.get_non_infinite_y_list(t[2])))
 
     lines = zip(all_styles, sorted_vals_by_tool)
 
