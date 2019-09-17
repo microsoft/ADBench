c21f9d3fd7be74c024c9c904be96f1c55b3fac3a
Ilia Egorov <egorovIlyak@yandex.ru>
2019-09-17 18:46:00 +0300

    module loader rewrited

--- src/python/runner/ModuleLoader.py
+++ src/python/runner/ModuleLoader.py
@@ -1,18 +1,30 @@
 import importlib.util
+import gc
 
 class ModuleLoader:
     def __init__(self, module_path):
         self.test = None
+        self._class = None
         self.module_path = module_path
 
     # return test specified by module_path
     def get_test(self):
         if self.test != None:
-            del self.test # DANGER here might be unexpected error: after deletion of self.test it might be created locally in get_test scope.
-        spec = importlib.util.spec_from_file_location("get_test", self.module_path)
-        self.test = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(self.test)
-        if self.test != None:
-            return self.test
+            del self.test # DANGER here might be some error after deletion of self.test
+            gc.collect() # force run garbage collector
+
+        # Module name should be the same as class name in that module
+        # Get class name from args
+        class_name = self.module_path.rsplit('/')[-1].rsplit('.')[0]
+
+        # import module
+        spec = importlib.util.spec_from_file_location(class_name, self.module_path)
+        if spec != None:
+            self.test = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(self.test)
+
+            # get class from module
+            self._class = getattr(self.test, class_name)
+            return self._class
         else:
             raise RuntimeError("Can't load module with path: " + self.module_path)
