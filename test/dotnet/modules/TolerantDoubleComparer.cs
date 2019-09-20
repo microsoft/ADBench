74057943eea817a17e96855bada7a069013bd698
Alexander Novikov <b-alnovi@microsoft.com>
2019-09-20 12:12:00 +0300

    diffsharp tests

---
+++ test/dotnet/modules/TolerantDoubleComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotnetTests
+{
+    class TolerantDoubleComparer : IEqualityComparer<double>
+    {
+        readonly double tolerance;
+
+        public TolerantDoubleComparer(double tolerance)
+        {
+            this.tolerance = tolerance;
+        }
+
+        public static TolerantDoubleComparer FromTolerance(double tolerance) => new TolerantDoubleComparer(tolerance);
+
+        public bool Equals(double x, double y) => Math.Abs(x - y) < tolerance;
+
+        public int GetHashCode(double obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
