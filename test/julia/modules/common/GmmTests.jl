724024a5c110fc249758bf4b114a9863c264f453
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-09-20 17:10:26 +0300

    Julia tests now expect greater precision.

--- test/julia/modules/common/GmmTests.jl
+++ test/julia/modules/common/GmmTests.jl
@@ -1,11 +1,11 @@
 module GmmTests
 
 dir = @__DIR__
-gmm_test_implementations = String[
-    "$dir/../../../../src/julia/modules/Zygote/ZygoteGMM.jl"
+gmm_test_implementations = Tuple{String, Float64}[
+    ("$dir/../../../../src/julia/modules/Zygote/ZygoteGMM.jl", 1e-8)
 ]
 
-map!(abspath, gmm_test_implementations, gmm_test_implementations)
+map!(tup -> (abspath(tup[1]), tup[2]), gmm_test_implementations, gmm_test_implementations)
 
 using Test
 include("../../../../src/julia/shared/load.jl")
@@ -18,7 +18,7 @@ using TestUtils
 
 @testset "GMM Module Tests" begin
 
-@testset "GMM Module Test ($(basename(module_path)))" for module_path in gmm_test_implementations
+@testset "GMM Module Test ($(basename(module_path)))" for (module_path, tolerance) in gmm_test_implementations
     module_dir, module_filename = splitdir(module_path)
     module_name, module_ext = splitext(module_filename)
     # Assert that the path to the module is correct
@@ -39,14 +39,40 @@ using TestUtils
             output = empty_gmm_output()
             test.output!(output, test.context)
             # Objective is calculated correctly
-            @test 8.0738 ≈ output.objective atol=0.00001
+            @test 8.07380408004975791e+00 ≈ output.objective atol=tolerance
             test.calculate_jacobian!(test.context, 1)
             test.output!(output, test.context)
             # Gradient is calculated correctly
             @test 18 == size(output.gradient, 1)
-            correct_grad = [ 0.108663, -0.74127, 0.632607, 1.116926, 0.163333, -0.022, 0.227778, 1.20963, -0.06064, 2.5853, 0.11263, 0.38574, 0.07352, 5.41836, -0.3215, 1.71892, 0.86009, -0.99464 ]
+            correct_grad = [
+                1.08662855508652456e-01,
+                -7.41270039523898472e-01,
+                6.32607184015246071e-01,
+                1.11692576532787013e+00,
+                1.63333013551455269e-01,
+                -2.19989824071193142e-02,
+                2.27778292254236098e-01,
+                1.20963025612832187e+00,
+                -6.06375920733956339e-02,
+                2.58529994051162237e+00,
+                1.12632694524213789e-01,
+                3.85744309849611777e-01,
+                7.35180573182305508e-02,
+                5.41836362715595232e+00,
+                -3.21494409677446469e-01,
+                1.71892309775004937e+00,
+                8.60091090790866875e-01,
+                -9.94640930466322848e-01
+            ]
             for i ∈ 1:18
-               @test correct_grad[i] ≈ output.gradient[i] atol=0.00001
+               @test correct_grad[i] ≈ output.gradient[i] atol=tolerance
+            end
+            test.calculate_jacobian!(test.context, 3)
+            test.output!(output, test.context)
+            # Gradient is calculated correctly for times = 3
+            @test 18 == size(output.gradient, 1)
+            for i ∈ 1:18
+               @test correct_grad[i] ≈ output.gradient[i] atol=tolerance
             end
             # Objective runs multiple times
             @test can_objective_run_multiple_times!(test.context, test.calculate_objective!)
