8b002f0294c0e66e64f27370375dc207606b5364
Alexander Novikov <b-alnovi@microsoft.com>
2019-09-27 15:42:28 +0300

    diffsharp calculation unit tests

--- test/dotnet/modules/BATests.cs
+++ test/dotnet/modules/BATests.cs
@@ -2,7 +2,7 @@ using DotnetRunner;
 using DotnetRunner.Data;
 using Xunit;
 
-namespace DotnetTests
+namespace DotnetModulesTests
 {
     public class BATests
     {
@@ -34,12 +34,12 @@ namespace DotnetTests
 
             for (int i = 0; i < 20; i += 2)
             {
-                Assert.Equal(-0.26904884923518940, output.ReprojErr[i], comparer);
-                Assert.Equal(0.25994479267790188, output.ReprojErr[i + 1], comparer);
+                Assert.Equal(-2.69048849235189402e-01, output.ReprojErr[i], comparer);
+                Assert.Equal(2.59944792677901881e-01, output.ReprojErr[i + 1], comparer);
             }
             for (int i = 0; i < 10; i++)
             {
-                Assert.Equal(0.82609265151599998, output.WErr[i], comparer);
+                Assert.Equal(8.26092651515999976e-01, output.WErr[i], comparer);
             }
         }
 
@@ -61,40 +61,39 @@ namespace DotnetTests
             Assert.Equal(31, output.J.rows.Count);
             Assert.Equal(310, output.J.cols.Count);
             Assert.Equal(310, output.J.vals.Count);
-            Assert.Equal(228.877, output.J.vals[0], comparer);
-            Assert.Equal(634.575, output.J.vals[1], comparer);
-            Assert.Equal(-782.223, output.J.vals[2], comparer);
-            Assert.Equal(2.42893, output.J.vals[3], comparer);
-            Assert.Equal(-11.7828, output.J.vals[4], comparer);
-            Assert.Equal(2.54169, output.J.vals[5], comparer);
-            Assert.Equal(-1.03657, output.J.vals[6], comparer);
-            Assert.Equal(0.417022, output.J.vals[7], comparer);
+            Assert.Equal(2.28877202208246757e+02, output.J.vals[0], comparer);
+            Assert.Equal(6.34574811495545418e+02, output.J.vals[1], comparer);
+            Assert.Equal(-7.82222866259340549e+02, output.J.vals[2], comparer);
+            Assert.Equal(2.42892615607159668e+00, output.J.vals[3], comparer);
+            Assert.Equal(-1.17828079628011313e+01, output.J.vals[4], comparer);
+            Assert.Equal(2.54169312487743460e+00, output.J.vals[5], comparer);
+            Assert.Equal(-1.03657084958518086e+00, output.J.vals[6], comparer);
+            Assert.Equal(4.17022000000000004e-01, output.J.vals[7], comparer);
             Assert.Equal(0.0, output.J.vals[8], comparer);
-            Assert.Equal(-350.74, output.J.vals[9], comparer);
-            Assert.Equal(-912.108, output.J.vals[10], comparer);
-            Assert.Equal(-2.42893, output.J.vals[11], comparer);
-            Assert.Equal(11.7828, output.J.vals[12], comparer);
-            Assert.Equal(-0.834044, output.J.vals[307], comparer);
-            Assert.Equal(-0.834044, output.J.vals[308], comparer);
-            Assert.Equal(-0.834044, output.J.vals[309], comparer);
+            Assert.Equal(-3.50739521096005205e+02, output.J.vals[9], comparer);
+            Assert.Equal(-9.12107773668008576e+02, output.J.vals[10], comparer);
+            Assert.Equal(-2.42892615607159668e+00, output.J.vals[11], comparer);
+            Assert.Equal(1.17828079628011313e+01, output.J.vals[12], comparer);
+            Assert.Equal(-8.34044000000000008e-01, output.J.vals[307], comparer);
+            Assert.Equal(-8.34044000000000008e-01, output.J.vals[308], comparer);
+            Assert.Equal(-8.34044000000000008e-01, output.J.vals[309], comparer);
         }
-
-        /*
-        
-        TEST_P(BaModuleTest, ObjectiveRunsMultipleTimes)
+/*
+        [Fact]
+        public void ObjectiveRunsMultipleTimes()
         {
-            auto module = moduleLoader->get_ba_test();
-            ASSERT_NE(module, nullptr);
-            BAInput input;
+            var module = moduleLoader.GetBATest();
+            Assert.NotNull(module);
 
             // Read instance
-            read_ba_instance("batest.txt", input.n, input.m, input.p,
-                input.cams, input.X, input.w, input.obs, input.feats);
-            module->prepare(std::move(input));
+            var input = DataLoader.ReadBAInstance("batest.txt");
+
+            module.Prepare(input);
 
             EXPECT_TRUE(can_objective_run_multiple_times(*module, &ITest<BAInput, BAOutput>::calculate_objective));
         }
-
+     
+    /*
         TEST_P(BaModuleTest, JacobianRunsMultipleTimes)
         {
             auto module = moduleLoader->get_ba_test();
