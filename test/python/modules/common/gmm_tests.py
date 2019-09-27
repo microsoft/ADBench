32022690b318ca3ad8cb5bb7a1a87f6686aa0f91
Mikhail Nikolaev (Lomonosov Moscow State) <v-miniko@microsoft.com>
2019-09-27 18:59:21 +0300

    GMM common tests for Python modules implementing

--- test/python/modules/common/gmm_tests.py
+++ test/python/modules/common/gmm_tests.py
@@ -17,7 +17,7 @@ sys.path.append(
 )
 
 from runner.ModuleLoader import module_load
-from shared.input_utils import read_ba_instance
+from shared.input_utils import read_gmm_instance
 import utils
 
 # root directory of python modules
@@ -37,44 +37,33 @@ MODULES_DIR = os.path.abspath(os.path.join(
 # Parameters for different modules. They have the next form:
 # {
 #   "path": <module path relative to src/python/modules directory>,
-#   "tolerance": <tolerance for module output results>
+#   "tolerance": <tolerance for module res results>
 # }
 test_params = [
     {
-        "path": os.path.join("PyTorch", "PyTorchBA.py"),
+        "path": os.path.join("PyTorch", "PyTorchGMM.py"),
         "tolerance": 1e-8
     }
 ]
 
 
 
-class PythonModuleCommonBATests(utils.BaseTestClass):
-    '''Checking BA objective differentiation in all python modules.''' 
+class PythonModuleCommonGMMTests(utils.BaseTestClass):
+    '''Checking GMM objective differentiation in all python modules.''' 
 
     # helping functions
     def objective_calculation_correctness(self, times):
         '''Checks objective calculation correctness running calculation
         several times.'''
 
-        input = read_ba_instance("batest.txt")
+        input = read_gmm_instance("gmmtest.txt", False)
         self.test.prepare(input)
         self.test.calculate_objective(times)
         output = self.test.output()
 
-        expected_weight_err = np.full(10, 8.26092651515999976e-01)
-        expected_reproj_err = np.array(
-            [ -2.69048849235189402e-01, 2.59944792677901881e-01 ] * 10
-        )
-
-        self.assertFloatArrayEqual(
-            expected_reproj_err,
-            output.reproj_err,
-            self.params["tolerance"]
-        )
-
-        self.assertFloatArrayEqual(
-            expected_weight_err,
-            output.w_err,
+        self.assertFloatEqual(
+            8.07380408004975791e+00,
+            output.objective,
             self.params["tolerance"]
         )
 
@@ -82,36 +71,37 @@ class PythonModuleCommonBATests(utils.BaseTestClass):
         '''Checks jacobian calculation correctness running calculation
         several times.'''
 
-        input = read_ba_instance("batest.txt")
+        input = read_gmm_instance("gmmtest.txt", False)
         self.test.prepare(input)
         self.test.calculate_jacobian(times)
         output = self.test.output()
 
-        # check jacobian shape
-        self.assertEqual(30, output.J.nrows)
-        self.assertEqual(62, output.J.ncols)
-        self.assertEqual(31, len(output.J.rows))
-        self.assertEqual(310, len(output.J.cols))
-        self.assertEqual(310, len(output.J.vals))
+        expected_gradient = [
+            1.08662855508652456e-01,
+            -7.41270039523898472e-01,
+            6.32607184015246071e-01,
+            1.11692576532787013e+00,
+            1.63333013551455269e-01,
+            -2.19989824071193142e-02,
+            2.27778292254236098e-01,
+            1.20963025612832187e+00,
+            -6.06375920733956339e-02,
+            2.58529994051162237e+00,
+            1.12632694524213789e-01,
+            3.85744309849611777e-01,
+            7.35180573182305508e-02,
+            5.41836362715595232e+00,
+            -3.21494409677446469e-01,
+            1.71892309775004937e+00,
+            8.60091090790866875e-01,
+            -9.94640930466322848e-01
+        ]
 
-        # check jacobian values
-        eps = self.params["tolerance"]
-        self.assertFloatEqual(2.28877202208246757e+02, output.J.vals[0], eps)
-        self.assertFloatEqual(6.34574811495545418e+02, output.J.vals[1], eps)
-        self.assertFloatEqual(-7.82222866259340549e+02, output.J.vals[2], eps)
-        self.assertFloatEqual(2.42892615607159668e+00, output.J.vals[3], eps)
-        self.assertFloatEqual(-1.17828079628011313e+01, output.J.vals[4], eps)
-        self.assertFloatEqual(2.54169312487743460e+00, output.J.vals[5], eps)
-        self.assertFloatEqual(-1.03657084958518086e+00, output.J.vals[6], eps)
-        self.assertFloatEqual(4.17022000000000004e-01, output.J.vals[7], eps)
-        self.assertFloatEqual(0.0, output.J.vals[8], eps)
-        self.assertFloatEqual(-3.50739521096005205e+02, output.J.vals[9], eps)
-        self.assertFloatEqual(-9.12107773668008576e+02, output.J.vals[10], eps)
-        self.assertFloatEqual(-2.42892615607159668e+00, output.J.vals[11], eps)
-        self.assertFloatEqual(1.17828079628011313e+01, output.J.vals[12], eps)
-        self.assertFloatEqual(-8.34044000000000008e-01, output.J.vals[307], eps)
-        self.assertFloatEqual(-8.34044000000000008e-01, output.J.vals[308], eps)
-        self.assertFloatEqual(-8.34044000000000008e-01, output.J.vals[309], eps)
+        self.assertFloatArrayEqual(
+            expected_gradient,
+            output.gradient,
+            self.params["tolerance"]
+        )
 
 
 
@@ -149,7 +139,7 @@ class PythonModuleCommonBATests(utils.BaseTestClass):
     def test_objective_runs_multiple_times(self):
         '''Checks if objective can be calculated nultiple times.'''
 
-        input = read_ba_instance("batest.txt")
+        input = read_gmm_instance("gmmtest.txt", False)
         self.test.prepare(input)
 
         func = self.test.calculate_objective
@@ -158,7 +148,7 @@ class PythonModuleCommonBATests(utils.BaseTestClass):
     def test_jacobian_runs_multiple_times(self):
         '''Checks if jacobian can be calculated nultiple times.'''
 
-        input = read_ba_instance("batest.txt")
+        input = read_gmm_instance("gmmtest.txt", False)
         self.test.prepare(input)
 
         func = self.test.calculate_jacobian
@@ -170,7 +160,7 @@ if __name__ == "__main__":
     suite = unittest.TestSuite()
     for param_set in test_params:
         suite.addTest(utils.ParametrizedTestClass.parametrize(
-            PythonModuleCommonBATests,
+            PythonModuleCommonGMMTests,
             params = param_set
         ))
 
