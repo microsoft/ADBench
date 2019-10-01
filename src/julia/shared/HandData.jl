15bc9a925c898e0a94d6bf54e22182c499d45ccd
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-10-01 18:30:09 +0300

    Parsing hand input in julia.

--- src/julia/shared/HandData.jl
+++ src/julia/shared/HandData.jl
@@ -1,6 +1,7 @@
 module HandData
+using DelimitedFiles
 
-export HandModel, HandInput, HandOutput, empty_hand_output
+export HandModel, HandInput, HandOutput, empty_hand_output, load_hand_input
 
 struct HandModel
     bone_names::Vector{String}
@@ -18,7 +19,7 @@ struct HandInput
     points::Matrix{Float64}
     theta::Vector{Float64}
     "Is present only for 'complicated' kind of problems."
-    us::Vector{Float64}
+    us::Union{Matrix{Float64}, Nothing}
 end
 
 mutable struct HandOutput
@@ -27,5 +28,75 @@ mutable struct HandOutput
 end
 
 empty_hand_output() = HandOutput([], [])
+
+function load_hand_model(model_dir::AbstractString)::HandModel
+    delimeter = ':'
+    bones_fn = joinpath(model_dir, "bones.txt")
+    A = readdlm(bones_fn, delimeter)
+
+    n_bones = size(A, 1)
+    bone_names = A[:, 1]
+    parents = A[:, 2] .+ 1 #julia indexing
+
+    transforms = A[:, 3:18]
+    transforms = permutedims(reshape(transforms, (n_bones, 4, 4)), (1, 3, 2))
+
+    inverse_absolute_transforms  = A[:, 19:34]
+    inverse_absolute_transforms = permutedims(reshape(inverse_absolute_transforms, (n_bones, 4, 4)), (1, 3, 2));
+
+    vertices_fn = joinpath(model_dir, "vertices.txt")
+    A = readdlm(vertices_fn, delimeter);
+
+    base_positions = A[:, 1:3]';
+    n_vertices = size(A, 1);
+
+    weights = zeros(Float64, n_bones, n_vertices);
+    for i_vert = 1:n_vertices
+        for i=0:A[i_vert, 9]-1
+            i_bone = A[i_vert, 9 + i * 2 + 1] + 1; #julia indexing
+            weights[i_bone, i_vert] = A[i_vert, 9 + i * 2 + 2];
+        end
+    end
+
+    HandModel(
+        bone_names,
+        parents,
+        transforms,
+        inverse_absolute_transforms,
+        [base_positions; ones(Float64, 1, size(base_positions, 2))],
+        weights,
+        false
+    )
+end
+
+function load_hand_input(fn::AbstractString, iscomplicated::Bool)::HandInput
+    modeldir = joinpath(dirname(fn), "model")
+    model = load_hand_model(modeldir)
+    open(fn) do io
+        line = split(readline(io), " ")
+        n_pts = parse(Int, line[1])
+        n_theta = parse(Int, line[2])
+        
+        corrs = Vector{Int}(undef, n_pts)
+        pts = Matrix{Float64}(undef, 3, n_pts)
+        for i in 1:n_pts
+            line = split(readline(io), " ")
+            corrs[i] = parse(Int, line[1]) + 1 #julia indexing
+            for j in 1:3
+                pts[j, i] = parse(Float64, line[j + 1])
+            end
+        end
+
+        if iscomplicated
+            us = hcat([parse.(Float64, split(readline(io), " ")) for i=1:n_pts]...)
+        else
+            us = nothing
+        end
+
+        theta = [parse(Float64, readline(io)) for i=1:n_theta]
+
+        return HandInput(model, corrs, pts, theta, us)
+    end
+end
 
 end
