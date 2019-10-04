cca78b0c604a274870c90944b5eaf0fe02f0937a
Mikhail Nikolaev (Lomonosov Moscow State) <v-miniko@microsoft.com>
2019-10-04 20:06:45 +0300

    LSTM objective test class implementing

---
+++ src/python/modules/Tensorflow/TensorflowLSTM.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import, division, print_function, \
+                       unicode_literals
+
+import tensorflow as tf
+import numpy as np
+
+from modules.Tensorflow.utils import to_tf_tensor
+from shared.ITest import ITest
+from shared.LSTMData import LSTMInput, LSTMOutput
+from modules.Tensorflow.lstm_objective import lstm_objective
+
+
+
+class TensorflowLSTM(ITest):
+    '''Test class for LSTM diferentiation by Tensorflow.'''
+
+    def prepare(self, input):
+        '''Prepares calculating. This function must be run before any others.'''
+
+        self.main_params = to_tf_tensor(input.main_params)
+        self.extra_params = to_tf_tensor(input.extra_params)
+        self.state = to_tf_tensor(input.state)
+        self.sequence = to_tf_tensor(input.sequence)
+
+        self.gradient = tf.zeros(0)
+        self.objective = tf.zeros(1)
+
+    def output(self):
+        '''Returns calculation result.'''
+
+        return LSTMOutput(self.objective.numpy(), self.gradient.numpy())
+
+    def calculate_objective(self, times):
+        '''Calculates objective function many times.'''
+
+        for _ in range(times):
+            self.objective = lstm_objective(
+                self.main_params,
+                self.extra_params,
+                self.state,
+                self.sequence
+            )
+
+    def calculate_jacobian(self, times):
+        ''' Calculates objective function jacobian many times.'''
+
+        for _ in range(times):
+            with tf.GradientTape(persistent = True) as t:
+                t.watch(self.main_params)
+                t.watch(self.extra_params)
+
+                self.objective = lstm_objective(
+                    self.main_params,
+                    self.extra_params,
+                    self.state,
+                    self.sequence
+                )
+
+            dmain_params = t.gradient(self.objective, self.main_params),
+            dextra_params = t.gradient(self.objective, self.extra_params)
+            self.gradient = tf.concat((
+                tf.reshape(dmain_params, [-1]),
+                tf.reshape(dextra_params, [-1])
+            ), 0)
