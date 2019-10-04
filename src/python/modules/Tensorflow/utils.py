66dfae2cbe7ead440b987215ba5f45bbff30a9eb
Mikhail Nikolaev (Lomonosov Moscow State) <v-miniko@microsoft.com>
2019-10-04 20:06:45 +0300

    LSTM objective test class implementing

--- src/python/modules/Tensorflow/utils.py
+++ src/python/modules/Tensorflow/utils.py
@@ -2,6 +2,22 @@ import tensorflow as tf
 
 
 
+def to_tf_tensor(ndarray, dtype = tf.float64):
+    '''Converts the given multidimensional array to a tensorflow tensor.
+
+    Args:
+        ndarray (ndarray-like): parameter for conversion.
+        dtype (type, optional): defines a type of tensor elements. Defaults to
+            tf.float64.
+
+    Returns:
+        tensorflow tensor
+    '''
+
+    return tf.convert_to_tensor(ndarray, dtype = dtype)
+
+
+
 def shape(tf_tensor):
     '''Returns shape of a tensorflow tensor like a list if integers.'''
 
