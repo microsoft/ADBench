128b7ba82b09e25ef3208ffdc7ec9a7b3624061d
Mikhail Nikolaev (Lomonosov Moscow State) <v-miniko@microsoft.com>
2019-10-08 15:33:50 +0300

    GmmTests jacobian checking refactoring

--- test/cpp/modules/common/GmmTests.cpp
+++ test/cpp/modules/common/GmmTests.cpp
@@ -42,25 +42,32 @@ void GmmModuleTest::jacobian_calculation_correctness(int n_run_times)
     module->calculate_jacobian(n_run_times);
 
     auto output = module->output();
+
+    std::vector<double> expected_gradient
+    {
+        1.08662855508652456e-01,
+        -7.41270039523898472e-01,
+        6.32607184015246071e-01,
+        1.11692576532787013e+00,
+        1.63333013551455269e-01,
+        -2.19989824071193142e-02,
+        2.27778292254236098e-01,
+        1.20963025612832187e+00,
+        -6.06375920733956339e-02,
+        2.58529994051162237e+00,
+        1.12632694524213789e-01,
+        3.85744309849611777e-01,
+        7.35180573182305508e-02,
+        5.41836362715595232e+00,
+        -3.21494409677446469e-01,
+        1.71892309775004937e+00,
+        8.60091090790866875e-01,
+        -9.94640930466322848e-01
+    };
+
     EXPECT_EQ(18, output.gradient.size());
-    EXPECT_NEAR(1.08662855508652456e-01, output.gradient[0], epsilon);
-    EXPECT_NEAR(-7.41270039523898472e-01, output.gradient[1], epsilon);
-    EXPECT_NEAR(6.32607184015246071e-01, output.gradient[2], epsilon);
-    EXPECT_NEAR(1.11692576532787013e+00, output.gradient[3], epsilon);
-    EXPECT_NEAR(1.63333013551455269e-01, output.gradient[4], epsilon);
-    EXPECT_NEAR(-2.19989824071193142e-02, output.gradient[5], epsilon);
-    EXPECT_NEAR(2.27778292254236098e-01, output.gradient[6], epsilon);
-    EXPECT_NEAR(1.20963025612832187e+00, output.gradient[7], epsilon);
-    EXPECT_NEAR(-6.06375920733956339e-02, output.gradient[8], epsilon);
-    EXPECT_NEAR(2.58529994051162237e+00, output.gradient[9], epsilon);
-    EXPECT_NEAR(1.12632694524213789e-01, output.gradient[10], epsilon);
-    EXPECT_NEAR(3.85744309849611777e-01, output.gradient[11], epsilon);
-    EXPECT_NEAR(7.35180573182305508e-02, output.gradient[12], epsilon);
-    EXPECT_NEAR(5.41836362715595232e+00, output.gradient[13], epsilon);
-    EXPECT_NEAR(-3.21494409677446469e-01, output.gradient[14], epsilon);
-    EXPECT_NEAR(1.71892309775004937e+00, output.gradient[15], epsilon);
-    EXPECT_NEAR(8.60091090790866875e-01, output.gradient[16], epsilon);
-    EXPECT_NEAR(-9.94640930466322848e-01, output.gradient[17], epsilon);
+    for (int i = 0; i < expected_gradient.size(); i++)
+        EXPECT_NEAR(expected_gradient[i], output.gradient[i], epsilon);
 }
 
 
