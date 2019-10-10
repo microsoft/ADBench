ca4d71a102a49e93d133cec3e4778d09be30b726
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-10-10 17:16:22 +0300

    Parametrized module tests; prettification.

---
+++ test/dotnet/modules/ModuleTestParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetModulesTests
+{
+    public struct ModuleTestParameters
+    {
+        public string ModuleName { get; }
+        public double Tolerance { get; }
+
+        public ModuleTestParameters(string moduleName, double tolerance)
+        {
+            ModuleName = moduleName;
+            Tolerance = tolerance;
+        }
+    }
+}
