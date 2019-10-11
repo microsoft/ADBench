2f0fffada1f27349016c0f093c31fea32e5b4b20
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-10-11 12:16:47 +0300

    Added some doc comments.

--- src/dotnet/runner/ITest.cs
+++ src/dotnet/runner/ITest.cs
@@ -4,13 +4,35 @@ using System.Text;
 
 namespace DotnetRunner
 {
+    /// <summary>
+    /// Automatic differentiation performance testing module for an objective
+    /// corresponding to given Input and Output types. Differentiates the objective
+    /// using some AD framework.
+    /// </summary>
     public interface ITest<TInput, TOutput>
     {
-        // This function must be called before any other function.
+        /// <summary>
+        /// Converts the input data from the <typeparamref name="TInput"/> type
+        /// in which it is provided by the calling benchmark runner into
+        /// the format optimized for use with the tested AD framework.
+        /// Stores it.
+        /// 
+        /// Optionally, performs other preparatory activities need by the tested AD framework.
+        /// </summary>
         void Prepare(TInput input);
-        // calculate function
+        /// <summary>
+        /// Repeatedly computes the objective function <paramref name="times"/> times for the stored input.
+        /// Stores results.
+        /// </summary>
         void CalculateObjective(int times);
+        /// <summary>
+        /// Repeatedly computes the jacobian of the objective function <paramref name="times"/> times
+        /// for the stored input. Stores results.
+        /// </summary>
         void CalculateJacobian(int times);
+        /// <summary>
+        /// Converts the stored outputs to <typeparamref name="TOutput"/>.
+        /// </summary>
         TOutput Output();
     }
 }
