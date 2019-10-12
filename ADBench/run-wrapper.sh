8aaf1b60afef640441d6438be03d4c14348c0537
Ilia Egorov <egorovIlyak@yandex.ru>
2019-10-12 20:29:33 +0300

    wrapper shell script for docker

---
+++ ADBench/run-wrapper.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+RUN=0
+PLOT=0
+USAGE_MESSAGE="Usage: ./run-wrapper.sh ([-r|--run-all] || [-p|--plot-graphs] || [-h|--help]) args"
+
+case $1 in
+    -r|--run-all)
+        RUN=1
+        shift
+    ;;
+    -p|--plot-graphs)
+        PLOT=1
+        shift
+    ;;
+    -h|--help)
+        echo $USAGE_MESSAGE
+        exit 0
+    ;;
+    *)
+        echo $USAGE_MESSAGE
+        exit 0
+    ;;
+esac
+
+if ((RUN)) ; then
+    echo "Executing:" "pwsh" "run-all.ps1" "$@"
+    "pwsh" "run-all.ps1" "$@"
+fi
+
+if ((PLOT)) ; then
+    echo "Executing:" "python3" "plot_graphs.py" "$@"
+    "python3" "plot_graphs.py" "$@"
+fi
