11bc1fbc3cfc4b9ad53a5207325f7c5696518c18
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-10-15 16:15:47 +0300

    dotnet runner supports LSTM.

---
+++ src/dotnet/runner/Benchmarks/LSTMBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using DotnetRunner.Data;
+
+namespace DotnetRunner.Benchmarks
+{
+    public class LSTMBenchmark : Benchmark<LSTMInput, LSTMOutput>
+    {
+        protected override ITest<LSTMInput, LSTMOutput> GetTest(ModuleLoader moduleLoader) => moduleLoader.GetLSTMTest();
+
+        protected override LSTMInput ReadInputData(string inputFilePath, DefaultParameters parameters) => DataLoader.ReadLSTMInstance(inputFilePath);
+
+        protected override void SaveOutputToFile(LSTMOutput output, string outputPrefix, string input_basename, string module_basename)
+        {
+            SavingOutput.SaveValueToFile(SavingOutput.ObjectiveFileName(outputPrefix, input_basename, module_basename), output.Objective);
+            SavingOutput.SaveVectorToFile(SavingOutput.JacobianFileName(outputPrefix, input_basename, module_basename), output.Gradient);
+        }
+    }
+}
