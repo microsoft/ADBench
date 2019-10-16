01397fbd58661061854de4b5a8312053d487db87
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-10-16 18:01:36 +0300

    Dotnet runner supports GMM.

---
+++ src/dotnet/runner/Benchmarks/GMMBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using DotnetRunner.Data;
+
+namespace DotnetRunner.Benchmarks
+{
+    public class GMMBenchmark : Benchmark<GMMInput, GMMOutput, GMMParameters>
+    {
+        protected override ITest<GMMInput, GMMOutput> GetTest(ModuleLoader moduleLoader) => moduleLoader.GetGMMTest();
+
+        protected override GMMInput ReadInputData(string inputFilePath, GMMParameters parameters) => DataLoader.ReadGMMInstance(inputFilePath, parameters.ReplicatePoint);
+
+        protected override void SaveOutputToFile(GMMOutput output, string outputPrefix, string input_basename, string module_basename)
+        {
+            SavingOutput.SaveValueToFile(SavingOutput.ObjectiveFileName(outputPrefix, input_basename, module_basename), output.Objective);
+            SavingOutput.SaveVectorToFile(SavingOutput.JacobianFileName(outputPrefix, input_basename, module_basename), output.Gradient);
+        }
+    }
+}
