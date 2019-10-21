fcb882777bb7dd54757956620235c16e645dd800
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2019-10-21 15:29:09 +0300

    Enable hand for diffsharp in run-all.

--- ADBench/run-all.ps1
+++ ADBench/run-all.ps1
@@ -325,7 +325,9 @@ Class Tool {
         } elseif ($this.type -eq [ToolType]::dotnet) {
             $cmd = "dotnet"
             $module_path = "$script:bindir/src/dotnet/modules/$($this.name)/$($this.name).dll"
-            $cmdargs = @("$script:bindir/src/dotnet/runner/DotnetRunner.dll $objective $module_path $dir_in$fn.txt $dir_out $script:minimum_measurable_time $script:nruns_f $script:nruns_J $script:time_limit")
+            $task = $objective.Split("-")[0]
+            if ($objective.contains("complicated")) { $task = "$task-Complicated" }
+            $cmdargs = @("$script:bindir/src/dotnet/runner/DotnetRunner.dll $task $module_path $dir_in$fn.txt $dir_out $script:minimum_measurable_time $script:nruns_f $script:nruns_J $script:time_limit")
         } elseif ($this.type -eq [ToolType]::py -or $this.type -eq [ToolType]::pybat) {
             $objective = $objective.ToLower().Replace("-", "_")
             if ($this.type -eq "py") { $cmd = "python" }
@@ -467,7 +469,7 @@ $tool_descriptors = @(
     [Tool]::new("Manual", "cpp", [ObjectiveType] "GMM, BA, Hand, LSTM", $false, 0.0)
     [Tool]::new("ManualEigen", "cpp", [ObjectiveType] "GMM, BA, Hand, LSTM", $true, $default_tolerance)
     [Tool]::new("ManualEigenVector", "cpp", [ObjectiveType] "GMM", $true, $default_tolerance)
-    [Tool]::new("DiffSharpModule", "dotnet", [ObjectiveType] "GMM, BA, LSTM", $true, $default_tolerance)
+    [Tool]::new("DiffSharpModule", "dotnet", [ObjectiveType] "GMM, BA, Hand, LSTM", $true, $default_tolerance)
     [Tool]::new("Tapenade", "cpp", [ObjectiveType] "BA, LSTM, GMM, Hand", $true, $default_tolerance)
     [Tool]::new("PyTorch", "python", [ObjectiveType] "BA, LSTM, GMM, Hand", $true, 1e-7)
     [Tool]::new("Tensorflow", "python", [ObjectiveType] "LSTM, GMM, BA", $true, $default_tolerance)
