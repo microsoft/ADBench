ce73cae06476325b12b56b1ef2d74936c74f634b
Mikhail Nikolaev (Lomonosov Moscow State) <v-miniko@microsoft.com>
2019-10-27 23:34:34 +0300

    ADBench web view application is implemented

---
+++ ADBench/ADBenchWebViewer/ADBenchWebViewer/Controllers/AzureBlobHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ADBenchWebViewer.Models;
+using Microsoft.Azure.Storage.Blob;
+
+namespace ADBenchWebViewer.Controllers
+{
+    /// <summary>
+    /// Contains helping methods for working with Azure Blob.
+    /// </summary>
+    public class AzureBlobHelper : IRunInfoProvider
+    {
+        private const string staticSubdirName = "static";
+        private const string plotlySubdirName = "plotly";
+
+        private readonly CloudBlobContainer rootContatiner;
+
+        public AzureBlobHelper(string containerAddress)
+        {
+            var containerUri = new Uri(containerAddress);
+            rootContatiner = new CloudBlobContainer(containerUri);
+        }
+
+        public IDictionary<string, RunInfo> GetRunsInfo()
+        {
+            return rootContatiner.ListBlobs()
+                .Where(item => item is CloudBlobDirectory)
+                .Select(item => item as CloudBlobDirectory)
+                .OrderByDescending(dir => dir.Prefix)
+                .ToDictionary(
+                    dir => dir.Prefix,
+                    dir =>
+                    {
+                        var parts = dir.Prefix.Trim('/').Split('_');
+                        return new RunInfo()
+                        {
+                            Date = parts[0].Replace('-', '.'),
+                            Time = parts[1].Replace('-', ':'),
+                            Commit = parts[2],
+                            CloudBlobDirectory = dir
+                        };
+                    }
+                );
+        }
+
+        public IDictionary<string, IEnumerable<PlotInfo>> GetPlotsInfo(string dirName)
+        {
+            var cloudDirectory = GetRunsInfo()[dirName].CloudBlobDirectory;
+            var staticPlotDir = cloudDirectory.GetDirectoryReference(staticSubdirName);
+            var plotlyPlotDir = cloudDirectory.GetDirectoryReference(plotlySubdirName);
+
+            var staticPlots = GetPlotBlobs(staticPlotDir);
+            var plotlyPlots = GetPlotBlobs(plotlyPlotDir);
+
+            return staticPlots
+                .Zip(plotlyPlots, (first, second) => (first.Key, val: (staticPlots: first.Value, plotlyPlots: second.Value)))
+                .ToDictionary(
+                    item => CapitalFirstLetter(item.Key),
+                    item => item.val.staticPlots
+                        .Zip(
+                            item.val.plotlyPlots,
+                            (st, pl) => new PlotInfo()
+                            {
+                                DisplayName = GetDisplayName(pl.Name),
+                                StaticPlotUrl = st.Uri.ToString(),
+                                PlotlyPlotUrl = pl.Uri.ToString()
+                            }
+                        )
+                );
+
+            IDictionary<string, IEnumerable<CloudBlob>> GetPlotBlobs(CloudBlobDirectory cloudDir) =>
+                cloudDir.ListBlobs()
+                    .Where(item => item is CloudBlobDirectory)
+                    .Select(item => item as CloudBlobDirectory)
+                    .ToDictionary(
+                        dir => dir.Prefix.Trim('/').Split('/').Last(),
+                        dir => dir.ListBlobs()
+                            .Where(sitem => sitem is CloudBlob)
+                            .Select(sitem => sitem as CloudBlob)
+                    );
+
+            string GetDisplayName(string name) => System.IO.Path.GetFileNameWithoutExtension(name.Split('/').Last());
+
+            string CapitalFirstLetter(string str) => Char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+}
---
+++ ADBench/ADBenchWebViewer/ADBenchWebViewer/Controllers/PlotsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADBenchWebViewer.Controllers
+{
+    public class PlotsController : Controller
+    {
+        private readonly IRunInfoProvider shareHelper;
+
+        public PlotsController(IRunInfoProvider shareHandler) => this.shareHelper = shareHandler;
+
+        public IActionResult Index()
+        {
+            var dirInfo = shareHelper.GetRunsInfo();
+            return View(dirInfo);
+        }
+
+        public IActionResult GetStatic(string dir)
+        {
+            return View(shareHelper.GetPlotsInfo(dir));
+        }
+
+        public IActionResult GetPlotly(string dir)
+        {
+            return View(shareHelper.GetPlotsInfo(dir));
+        }
+    }
+}
