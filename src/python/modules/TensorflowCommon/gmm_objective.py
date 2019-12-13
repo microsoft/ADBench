70647230edd1ba879692280cd64795b8acf520fa
Mikhail Nikolaev (Lomonosov Moscow State) <v-miniko@microsoft.com>
2019-12-13 18:31:02 +0300

    GMM objective refactoring for acceleration

--- src/python/modules/TensorflowCommon/gmm_objective.py
+++ src/python/modules/TensorflowCommon/gmm_objective.py
@@ -12,13 +12,17 @@ def logsumexp(x):
 
 
 
+def logsumexpvec(x):
+    '''The same as "logsumexp" but calculates result for each row separately.'''
+
+    mx = tf.reduce_max(x, 1)
+    lset = tf.reduce_logsumexp(tf.transpose(x) - mx, 0)
+    return tf.transpose(lset + mx)
+
+
+
 def log_gamma_distrib(a, p):
     return special.multigammaln(a, p)
-
-
-
-def sqsum(x):
-    return tf.reduce_sum(x ** 2)
 
 
 
@@ -26,20 +30,14 @@ def log_wishart_prior(p, wishart_gamma, wishart_m, sum_qs, Qdiags, icf):
     n = p + wishart_m + 1
     k = shape(icf)[0]
 
-    log2 = tf.math.log(tf.constant(2.0, dtype = tf.float64))
-    
-    out = tf.reduce_sum([
+    out = tf.reduce_sum(
         0.5 * wishart_gamma * wishart_gamma *
-            (sqsum(Qdiags[ik, :]) + sqsum(icf[ik, p:])) -
-            wishart_m * sum_qs[ik]
-
-        for ik in range(k)
-    ])
+        (tf.reduce_sum(Qdiags ** 2, 1) + tf.reduce_sum(icf[:, p:] ** 2, 1)) -
+        wishart_m * sum_qs
+    )
     
-    C = n * p * (math.log(wishart_gamma) - 0.5 * log2) - \
-        log_gamma_distrib(0.5 * n, p)
-
-    return out - k * C
+    C = n * p * (math.log(wishart_gamma / math.sqrt(2)))
+    return out - k * (C - log_gamma_distrib(0.5 * n, p))
 
 
 
@@ -49,14 +47,14 @@ def constructL(d, icf):
     def make_L_col(i):
         nelems = d - i - 1
         col = tf.concat([
-            tf.zeros(i + 1, dtype=tf.float64),
+            tf.zeros(i + 1, dtype = tf.float64),
             icf[constructL.Lparamidx:(constructL.Lparamidx + nelems)]
         ], 0)
 
         constructL.Lparamidx += nelems
         return col
 
-    columns = [ make_L_col(i) for i in range(d) ]
+    columns = tuple(make_L_col(i) for i in range(d))
     return tf.stack(columns, -1)
 
 
@@ -67,28 +65,22 @@ def Qtimesx(Qdiag, L, x):
 
 
 def gmm_objective(alphas, means, icf, x, wishart_gamma, wishart_m):
-    def inner_term(ix, ik):
-        xcentered = x[ix, :] - means[ik, :]
-        Lxcentered = Qtimesx(Qdiags[ik, :], Ls[ik, :, :], xcentered)
-        sqsum_Lxcentered = sqsum(Lxcentered)
-        return alphas[ik] + sum_qs[ik] - 0.5 * sqsum_Lxcentered
+    xshape = shape(x)
+    n = xshape[0]
+    d = xshape[1]
 
-    n = shape(x)[0]
-    d = shape(x)[1]
-    k = shape(alphas)[0]
-
-    Qdiags = tf.stack([ (tf.exp(icf[ik, :d])) for ik in range(k) ])
-    sum_qs = tf.stack([ (tf.reduce_sum(icf[ik, :d])) for ik in range(k) ])
+    Qdiags = tf.exp(icf[:, :d])
+    sum_qs = tf.reduce_sum(icf[:, :d], 1)
 
     icf_sz = shape(icf)[0]
-    Ls = tf.stack([ constructL(d, icf[i]) for i in range(icf_sz) ])
-    slse = tf.reduce_sum([
-        logsumexp(lse)
-        for lse in [
-            tf.stack([ inner_term(ix, ik) for ik in range(k) ])
-            for ix in range(n)
-        ]
-    ])
+    Ls = tf.stack(tuple( constructL(d, icf[i]) for i in range(icf_sz) ))
+
+    xcentered = tf.stack(tuple( x[i] - means for i in range(n) ))
+    Lxcentered = Qtimesx(Qdiags, Ls, xcentered)
+    sqsum_Lxcentered = tf.reduce_sum(Lxcentered ** 2, 2)
+    inner_term = alphas + sum_qs - 0.5 * sqsum_Lxcentered
+    lse = logsumexpvec(inner_term)
+    slse = tf.reduce_sum(lse)
 
     const = tf.constant(
         -n * d * 0.5 * math.log(2 * math.pi),
